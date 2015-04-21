6eecb2379399130fc759cf73ce985ba1bb955762
Sarah Nadi <nadi@st.informatik.tu-darmstadt.de>
2015-04-21 09:15:54 +0200

    resource file now found when running as a plugin

--- src/main/java/crossing/e1/featuremodel/clafer/ClaferModel.java
+++ src/main/java/crossing/e1/featuremodel/clafer/ClaferModel.java
@@ -19,59 +19,58 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
 import org.osgi.framework.Bundle;
 
+import crossing.e1.configurator.Activator;
 
 public class ClaferModel {
-	
+
 	private String modelName;
 	private AstModel model;
-	
-	public ClaferModel(){
+
+	public ClaferModel() {
 		loadModel();
 	}
-	
-	//temporarily hard coding model file
-	private void loadModel(){
+
+	// temporarily hard coding model file
+	private void loadModel() {
 		try {
-//			IWorkspaceRoot myWorkspaceRoot = ResourcesPlugin.getWorkspace().getRoot();
-//			IProject myWebProject = myWorkspaceRoot.getProject("ClaferConfigurator");
-//			if (myWebProject.exists() && !myWebProject.isOpen())
-//			      myWebProject.open(null);
-			
-	
-			//IFolder imagesFolder = myWebProject.getFolder("src/main/resources");
-			
-			//System.out.println("exists: " + imagesFolder.exists());
-			Triple<AstModel, Scope, Objective[]> pair = Javascript.readModel(new File(ClassLoader.getSystemResource("hashing.js").getFile()), Javascript.newEngine());
-            model = pair.getFst();
-            System.out.println("Loaded " + AstUtil.getNames(AstUtil.getClafers(model)) + ".");
+			Bundle bundle = Platform.getBundle(Activator.PLUGIN_ID);
+
+			Path originPath = new Path("src/main/resources/hashing.js");
+
+			URL bundledFileURL = FileLocator.find(bundle, originPath, null);
+
+			bundledFileURL = FileLocator.resolve(bundledFileURL);
+
+			Triple<AstModel, Scope, Objective[]> pair = Javascript.readModel(
+					new File(bundledFileURL.getFile()), Javascript.newEngine());
+			model = pair.getFst();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-//		} catch (CoreException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
 	}
-	
-	public List<AstConcreteClafer> getClafersByType(String type){
-		return model.getChildren().stream().filter(child -> child.getSuperClafer().getName().equals(type)).collect(Collectors.toList());
+
+	public List<AstConcreteClafer> getClafersByType(String type) {
+		return model.getChildren().stream()
+				.filter(child -> child.getSuperClafer().getName().equals(type))
+				.collect(Collectors.toList());
 	}
-	
+
 	public void setModelName(String modelName) {
 		this.modelName = modelName;
 	}
-	
+
 	public String getModelName() {
 		return modelName;
 	}
-	
+
 	public AstModel getModel() {
 		return model;
 	}
-
 
 }

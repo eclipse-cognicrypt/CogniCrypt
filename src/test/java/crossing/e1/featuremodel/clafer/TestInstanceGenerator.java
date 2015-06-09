a76f05301e8ff63306aad330f64cfb10bda3417a
rkamath3 <aramachandrakamath@gmail.com>
2015-06-09 14:31:51 +0200

    TestFile to generate instances of the model

---
+++ src/test/java/crossing/e1/featuremodel/clafer/TestInstanceGenerator.java
@@ -0,0 +1,14 @@
+package crossing.e1.featuremodel.clafer;
+
+import crossing.e1.configurator.wizard.InstanceGenerator;
+
+public class TestInstanceGenerator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			ClaferModel c=new ClaferModel();
+			System.out.println("Testing instance Generator method ");
+			InstanceGenerator instances=new InstanceGenerator();
+	}
+
+}

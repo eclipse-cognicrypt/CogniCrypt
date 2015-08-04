e8093a86d45e9a328d2e4506a9452a78362dfc30
rkamath3 <aramachandrakamath@gmail.com>
2015-08-04 21:07:47 +0200

    updated tests

---
+++ src/test/java/crossing/e1/featuremodel/clafer/test/TestCases.java
@@ -0,0 +1,116 @@
+package crossing.e1.featuremodel.clafer.test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.clafer.ast.AstAbstractClafer;
+import org.clafer.ast.AstClafer;
+import org.clafer.ast.AstConcreteClafer;
+
+import crossing.e1.configurator.ReadConfig;
+import crossing.e1.featuremodel.clafer.ClaferModel;
+import crossing.e1.featuremodel.clafer.InstanceGenerator;
+import crossing.e1.featuremodel.clafer.ParseClafer;
+import crossing.e1.featuremodel.clafer.StringLableMapper;
+/**
+ * @author Ram
+ *
+ */
+
+
+public class TestCases {
+
+	public static void main(String[] args) {
+		TestCases test = new TestCases();
+		String path = new ReadConfig().getClaferPath();
+		ClaferModel model = new ClaferModel(path);
+
+		//InstanceGenerator inst = test.getInstance(model);
+		//test.displayInstances(inst);
+		//test.displayConstraints(model);
+		//test.displaySuperClafers(model);
+		test.displayTasks(model);
+		test.displayProperties(model);
+
+	}
+	/**
+	 * @param model
+	 */
+	private void displayProperties(ClaferModel model) {
+
+		System.out.println("----- Listing Propertie -----");
+		ParseClafer parser= new ParseClafer();
+		for(String ins : model.getTaskList(model.getModel()).keySet()){
+			System.out.println("Task Name "+ins+"\nProperties are");
+			parser.getPrimitive(StringLableMapper.getTaskLables().get(ins));
+			for(AstConcreteClafer key : StringLableMapper.getPropertiesLables().keySet()){
+				System.out.println(key+" => "+StringLableMapper.getPropertiesLables().get(key));
+			}
+			StringLableMapper.resetProperties();
+		}
+		
+		
+	}
+	/**
+	 * @param model
+	 */
+	private void displayTasks(ClaferModel model) {
+		System.out.println("----- Listing Tasks -----");
+		for(String inst : StringLableMapper.getTaskLables().keySet()){
+			
+			System.out.println(inst+" => "+StringLableMapper.getTaskLables().get(inst));
+		}
+		
+	}
+	private void displaySuperClafers(ClaferModel model){
+
+		System.out.println("--Testing displaySuperClafers Method--");
+		Map<String, AstConcreteClafer> taskList=new HashMap<String, AstConcreteClafer>();
+		taskList=  model.getTaskList(model.getModel());
+		for(String key : taskList.keySet())
+		{
+			System.out.println("VALUE : "+ taskList.get(key));
+		}
+		
+	}
+
+//	private void displayConstraints(ClaferModel model) {
+//
+//		System.out.println("--Testing Constrainable properties--");
+//		for(String key : model.getPrimitive())
+//		{
+//			System.out.println("VALUE : "+ taskList.get(key));
+//		}
+//		
+//		for (AstConcreteClafer clafer : model.getConstraintClafers())
+//			System.out.println(clafer.toString());
+//
+//	}
+
+	Map<ArrayList<AstConcreteClafer>, ArrayList<Integer>>  getMap() {
+		Map<ArrayList<AstConcreteClafer>, ArrayList<Integer>>  filters = new HashMap<ArrayList<AstConcreteClafer>, ArrayList<Integer>>();
+//		filters.put(null, {3});
+//		filters.put(null, {256});
+		return filters;
+	}
+
+	void displayInstances(InstanceGenerator instance) {
+		System.out.println("----Diplaying instances----");
+		for (String b : instance.getInstances().keySet())
+			System.out.println(instance.displayInstanceValues(instance
+					.getInstances().get(b), ""));
+	}
+
+	InstanceGenerator getInstance(ClaferModel model) {
+		System.out.println("-- Testing instance Generator method--");
+		InstanceGenerator instance = new InstanceGenerator();
+		;
+		instance.generateInstances(model, getMap());
+		System.out.println("There are " + instance.getNoOfInstances()
+				+ " instances");
+		return instance;
+		// instance.displayInstances();
+	}
+
+}
---
+++ src/test/java/crossing/e1/featuremodel/clafer/test/TestDynamicModelUpdater.java
@@ -0,0 +1,47 @@
+package crossing.e1.featuremodel.clafer.test;
+/**
+ * 
+ */
+
+
+import java.util.List;
+
+import org.clafer.ast.*;
+import org.clafer.compiler.ClaferCompiler;
+import org.clafer.compiler.ClaferSolver;
+import org.clafer.instance.InstanceClafer;
+import org.clafer.scope.Scope;
+
+import crossing.e1.featuremodel.clafer.ClaferModel;
+import static org.clafer.ast.Asts.*;
+
+public class TestDynamicModelUpdater {
+	public static void main(String[] args) {
+		ClaferModel model = new ClaferModel("test.cfr");
+		List<AstConcreteClafer> people = model.getChildByName("person", model
+				.getModel().getChildren());
+		System.out.println("size:" + people.size());
+		for (AstConcreteClafer clafer : people) {
+			System.out.println(clafer);
+		}
+		ClaferSolver solver;
+		solver = ClaferCompiler.compile(model.getModel(),model.getTriple().getSnd() );
+		while (solver.find()) {
+			System.out.println(solver.instance());
+		}
+		System.out.println("total children "+model.getModel().getChildren().size());
+		System.out.println("No of instances before child "+solver.instanceCount());
+		System.out.println("AFTER Adding child");
+		AstConcreteClafer installation = model.getModel().addChild("Trudy")
+				.addChild("name").withCard(1, 1).refTo(people.get(0).getRef().getTargetType());
+				
+		solver = ClaferCompiler.compile(model.getModel(),model.getTriple().getSnd() );
+		while (solver.find()) {
+			System.out.println(solver.instance());
+		}
+		System.out.println("No of instances after child "+solver.instanceCount());
+	
+		System.out.println("total children "+model.getModel().getChildren().size());
+		
+	}
+}

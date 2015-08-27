f3738a162fc4bbea0a91ec8f9b3e7b4b1e627772
rkamath3 <aramachandrakamath@gmail.com>
2015-08-27 20:02:57 +0200

    TestFor Labels

--- src/test/java/crossing/e1/featuremodel/clafer/test/TestLables.java
+++ src/test/java/crossing/e1/featuremodel/clafer/test/TestLables.java
@@ -6,7 +6,33 @@ public class TestLables implements Lables{
 
 	public static void main(String[] args) {
 
-System.out.println(Lables.AVAILABLE_OPTIONS);
+		System.out.println("Lables.AVAILABLE_OPTIONS "+Lables.AVAILABLE_OPTIONS);
+		System.out.println("GREATER_THAN "+Lables.GREATER_THAN);
+		System.out.println("CLAFER_PATH "+Lables.CLAFER_PATH);
+		System.out.println("COMPLETE "+Lables.COMPLETE);
+		System.out.println("CONFIG_PATH "+Lables.CONFIG_PATH);
+		System.out.println("DESCRIPTION_INSTANCE_LIST_PAGE "+Lables.DESCRIPTION_INSTANCE_LIST_PAGE);
+		System.out.println("DESCRIPTION_TASK_SELECTION_PAGE "+Lables.DESCRIPTION_TASK_SELECTION_PAGE);
+		System.out.println("DESCRIPTION_TASK_SELECTION_PAGE "+Lables.DESCRIPTION_TASK_SELECTION_PAGE);
+		System.out.println("DESCRIPTION_VALUE_DISPLAY_PAGE "+Lables.DESCRIPTION_VALUE_DISPLAY_PAGE);
+		System.out.println("DESCRIPTION_VALUE_SELECTION_PAGE "+Lables.DESCRIPTION_VALUE_SELECTION_PAGE);
+		System.out.println("EQUALS "+Lables.EQUALS);
+		System.out.println("GREATER_THAN "+Lables.GREATER_THAN);
+		System.out.println("GREATER_THAN_EQUAL "+Lables.GREATER_THAN_EQUAL);
+		System.out.println("INSTANCE_ERROR_MESSGAE "+Lables.INSTANCE_ERROR_MESSGAE);
+		System.out.println("LABEL1 "+Lables.LABEL1);
+		System.out.println("LABEL2 "+Lables.LABEL2);
+		System.out.println("LESS_THAN "+Lables.LESS_THAN);
+		System.out.println("LESS_THAN_EQUAL "+Lables.LESS_THAN_EQUAL);
+		System.out.println("NO_TASK "+Lables.NO_TASK);
+		System.out.println("PLUGINID "+Lables.PLUGINID);
+		System.out.println("PROPERTIES "+Lables.PROPERTIES);
+		System.out.println("RESULT "+Lables.RESULT);
+		System.out.println("SECOND_PAGE "+Lables.SECOND_PAGE);
+		System.out.println("SELECT_PROPERTIES "+Lables.SELECT_PROPERTIES);
+		System.out.println("SELECT_TASK "+Lables.SELECT_TASK);
+		System.out.println("TASK_LIST "+Lables.TASK_LIST);
+		
 
 	}
 

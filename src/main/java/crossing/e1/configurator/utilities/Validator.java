876685c80d5e9bd5c5fcfcfb6f935b2fdffb883e
rkamath3 <aramachandrakamath@gmail.com>
2015-10-07 10:21:56 +0200

    updated basic user mode

--- src/main/java/crossing/e1/configurator/utilities/Validator.java
+++ src/main/java/crossing/e1/configurator/utilities/Validator.java
@@ -10,7 +10,7 @@ public class Validator {
 	/**
 	 * @return Validation method which will be invoked upon clicking next on the
 	 *         valueList page Next widgetPage is only accessible if there are
-	 *         more than 0 instances for a given clafer and the chosen values
+	 *         more than 0 instances for a given clafer & the chosen values
 	 */
 	public boolean validate(InstanceGenerator gen) {
 		if (gen.getNoOfInstances() > 0) {

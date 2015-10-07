fda00433a4b98d84debdc917790ed71f676beaa2
rkamath3 <aramachandrakamath@gmail.com>
2015-10-07 10:15:47 +0200

    Updated mywizard also merged window title

--- src/main/java/crossing/e1/configurator/wizard/MyWizard.java
+++ src/main/java/crossing/e1/configurator/wizard/MyWizard.java
@@ -4,14 +4,19 @@ import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.jface.wizard.WizardPage;
 
+import crossing.e1.configurator.Lables;
 import crossing.e1.configurator.ReadConfig;
+import crossing.e1.configurator.utilities.Validator;
 import crossing.e1.configurator.wizard.advanced.DisplayValuePage;
 import crossing.e1.configurator.wizard.advanced.ValueSelectionPage;
 import crossing.e1.configurator.wizard.beginner.DummyPage;
+import crossing.e1.configurator.wizard.beginner.QuestionsBeginner;
 import crossing.e1.configurator.wizard.beginner.RelevantQuestionsPage;
 import crossing.e1.cryptogen.generation.Generation;
 import crossing.e1.featuremodel.clafer.ClaferModel;
 import crossing.e1.featuremodel.clafer.InstanceGenerator;
+import crossing.e1.featuremodel.clafer.ParseClafer;
+import crossing.e1.featuremodel.clafer.StringLabelMapper;
 
 public class MyWizard extends Wizard {
 
@@ -19,15 +24,17 @@ public class MyWizard extends Wizard {
 	protected WizardPage valueListPage;
 	protected InstanceListPage instanceListPage;
 	protected DisplayValuePage finalValueListPage;
+	protected QuestionsBeginner quest;
 	private boolean advancedMode;
 	private ClaferModel claferModel;
-	InstanceGenerator instanceGenerator = new InstanceGenerator();
-	
-	private final Generation codeGeneration = new Generation();
 
+	InstanceGenerator instanceGenerator = new InstanceGenerator();
+
+	private final Generation codeGeneration = new Generation();
+	ParseClafer parser = new ParseClafer();
 	public MyWizard() {
 		super();
-		setWindowTitle("Configurator v1.0");
+		setWindowTitle("Cyrptography Task Configurator");
 		setNeedsProgressMonitor(true);
 		advancedMode = false;
 	}
@@ -38,6 +45,7 @@ public class MyWizard extends Wizard {
 		taskListPage = new TaskSelectionPage(claferModel);
 		this.setForcePreviousAndNextButtons(true);
 		addPage(taskListPage);
+		quest = new QuestionsBeginner();
 
 	}
 
@@ -53,33 +61,54 @@ public class MyWizard extends Wizard {
 	@Override
 	public IWizardPage getNextPage(IWizardPage currentPage) {
 		if (currentPage == taskListPage && taskListPage.canProceed()) {
-			
+
 			instanceGenerator.setTaskName(taskListPage.getValue());
 			instanceGenerator.setNoOfInstances(0);
-			
-			if(taskListPage.isAdvancedMode())
+
+			if (taskListPage.isAdvancedMode())
 				valueListPage = new ValueSelectionPage(null, claferModel);
-			else
-				valueListPage = new RelevantQuestionsPage(claferModel, taskListPage.getSelectedTask().getRelevantQuestions());
-				
+			else {
+				parser.setConstraintClafers(StringLabelMapper.getTaskLabels()
+						.get(taskListPage.getValue()));
+				quest.setTask(taskListPage.getValue());
+				quest.init();
+				if (quest.hasQuestions())
+					valueListPage = new DummyPage(quest);
+			}
 			addPage(valueListPage);
-			
+
 			return valueListPage;
-		} else if (currentPage == valueListPage) {
-			if (taskListPage.isAdvancedMode() && ((ValueSelectionPage) valueListPage).getPageStatus() == true){
-				if (((ValueSelectionPage) valueListPage).validate(instanceGenerator, claferModel)) {
+		} else if (currentPage.getTitle().equals("QuestionPage")) {
+			if (taskListPage.isAdvancedMode()
+					&& ((ValueSelectionPage) valueListPage).getPageStatus() == true) {
+				if (new Validator().validate(instanceGenerator)) {
 					instanceListPage = new InstanceListPage(instanceGenerator);
 					addPage(instanceListPage);
 					return instanceListPage;
 				}
-			}else if (!taskListPage.isAdvancedMode()){
-				//running in beginner mode
-				if(((RelevantQuestionsPage) valueListPage).validate(instanceGenerator, claferModel)){					
-					DummyPage dummyPage = new DummyPage();
-					addPage(dummyPage);
-					return dummyPage;
+			} else if (!taskListPage.isAdvancedMode() && !quest.hasQuestions()) {
+				// running in beginner mode
+				
+				quest.setMap(((DummyPage)currentPage).getSelection(), claferModel);
+				instanceGenerator.generateInstances(new ClaferModel(
+						new ReadConfig().getClaferPath()),
+						quest.getMap());
+
+				if (new Validator().validate(instanceGenerator)) {
+					instanceListPage = new InstanceListPage(instanceGenerator);
+					addPage(instanceListPage);
+					return instanceListPage;
+				} else {
+					// currentPage.(Lables.INSTANCE_ERROR_MESSGAE);
 				}
 			}
+			// else if(!taskListPage.isAdvancedMode() && quest.hasQuestions()){
+			// DummyPage dummyPage = new
+			// DummyPage(quest.nextQuestion(),quest.nextValues());
+			// addPage(dummyPage);
+			// System.out.println("Next page invoked");
+			// return dummyPage;
+			// }
 		} else if (currentPage == instanceListPage) {
 			finalValueListPage = new DisplayValuePage(
 					instanceListPage.getValue());
@@ -89,5 +118,4 @@ public class MyWizard extends Wizard {
 		return currentPage;
 
 	}
-
 }

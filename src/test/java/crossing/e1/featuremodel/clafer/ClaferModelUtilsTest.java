df8f338a9eb4dcb7f30bd2e2d96ee15bc14ab02d
kamathar <kamathar@pc-hiwi-34.sit.fraunhofer.de>
2015-10-22 14:01:17 +0200

    Test cases have been added for claferModelUtil and PropertiesMapperUtil

---
+++ src/test/java/crossing/e1/featuremodel/clafer/ClaferModelUtilsTest.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+package crossing.e1.featuremodel.clafer;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author kamathar
+ *
+ */
+public class ClaferModelUtilsTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link crossing.e1.featuremodel.clafer.ClaferModelUtils#getDisplayName(org.clafer.ast.AstClafer)}.
+	 */
+	@Test
+	public final void testGetDisplayName() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link crossing.e1.featuremodel.clafer.ClaferModelUtils#displayProperties(org.clafer.ast.AstClafer)}.
+	 */
+	@Test
+	public final void testDisplayProperties() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link crossing.e1.featuremodel.clafer.ClaferModelUtils#getDescription(org.clafer.ast.AstClafer)}.
+	 */
+	@Test
+	public final void testGetDescription() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link crossing.e1.featuremodel.clafer.ClaferModelUtils#isAbstract(org.clafer.ast.AstClafer)}.
+	 */
+	@Test
+	public final void testIsAbstract() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link crossing.e1.featuremodel.clafer.ClaferModelUtils#findClaferByName(org.clafer.ast.AstClafer, java.lang.String)}.
+	 */
+	@Test
+	public final void testFindClaferByName() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link crossing.e1.featuremodel.clafer.ClaferModelUtils#trimScope(java.lang.String)}.
+	 */
+	@Test
+	public final void testTrimScope() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
---
+++ src/test/java/crossing/e1/featuremodel/clafer/PropertiesMapperUtilTest.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package crossing.e1.featuremodel.clafer;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author kamathar
+ *
+ */
+public class PropertiesMapperUtilTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link crossing.e1.featuremodel.clafer.PropertiesMapperUtil#getTaskLabelsMap()}.
+	 */
+	@Test
+	public final void testGetTaskLabelsMap() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link crossing.e1.featuremodel.clafer.PropertiesMapperUtil#getPropertiesMap()}.
+	 */
+	@Test
+	public final void testGetPropertiesMap() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link crossing.e1.featuremodel.clafer.PropertiesMapperUtil#resetPropertiesMap()}.
+	 */
+	@Test
+	public final void testResetPropertiesMap() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link crossing.e1.featuremodel.clafer.PropertiesMapperUtil#getGroupPropertiesMap()}.
+	 */
+	@Test
+	public final void testGetGroupPropertiesMap() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link crossing.e1.featuremodel.clafer.PropertiesMapperUtil#resetGroupPropertiesMap()}.
+	 */
+	@Test
+	public final void testResetGroupPropertiesMap() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}

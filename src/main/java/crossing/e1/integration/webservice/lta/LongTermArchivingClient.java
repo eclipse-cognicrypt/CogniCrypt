5f8f7d9142030d6edfd5514a1b20b599d2bda355
mireif <reif@st.informatik.tu-darmstadt.de>
2016-08-11 14:23:11 +0200

    added some documentation

--- src/main/java/crossing/e1/integration/webservice/lta/LongTermArchivingClient.java
+++ src/main/java/crossing/e1/integration/webservice/lta/LongTermArchivingClient.java
@@ -9,16 +9,39 @@ import de.tu_darmstadt.cs.cdc.moltas.exceptions.ServiceClientCreationException;
 import de.tu_darmstadt.cs.cdc.moltas.model.archiving_system.Archive;
 import de.tu_darmstadt.cs.cdc.moltas.model.configuration.ArchiveConfiguration;
 import de.tu_darmstadt.cs.cdc.moltas.model.configuration.Scheme;
-import de.tu_darmstadt.cs.cdc.moltas.model.configuration.UpdateParameters;
 import de.tu_darmstadt.cs.cdc.moltas.services.ServiceType;
 import de.tu_darmstadt.cs.cdc.moltas.services.archiving_system.ArchivingSystem;
 import de.tu_darmstadt.cs.cdc.moltas.services.utilities.ServiceClientCreator;
 
-
+/**
+ * 
+ * A client that communicates with the REST API of the long term archiving system. The client
+ * allows to create, rename and delete archives.
+ * 
+ * @note The funcutionally of this class does only work within the university network of TU Darmstadt.
+ *
+ * 
+ * @author Michael Reif
+ *
+ */
 public class LongTermArchivingClient {
 
 	private ArchivingSystem archivingSystem;
 	
+	/**
+	 * 
+	 * Translates a `dataStructure` string to an ArchivingConfiguration.
+	 * 
+	 * Allowed dataStructures: 
+	 * 	- Simple_List
+	 *  - Skip_List
+	 *  - Merkle_Tree_Sequence
+	 *  - Notarial_Attestation_Wrapper
+	 * 
+	 * @param dataStructure DataStructe that fulfill the clafer constrains w.r.t. to the questionaire.
+	 * @return null, if the given dataStructure is unknown else the correct ArchiveConfiguration that
+	 * is required to create an Archive.
+	 */
 	public static ArchiveConfiguration mapDatastructureToScheme(String dataStructure){
 		Scheme archivingScheme = null;
 		switch(dataStructure){
@@ -36,11 +59,12 @@ public class LongTermArchivingClient {
 				break;
 		}
 		
-		ArchiveConfiguration archConfig = 
+		ArchiveConfiguration archConfig = (archivingScheme == null)? null : 
 				ArchiveConfiguration.createDefaultArchiveConfiguration(archivingScheme);
 		
 		return archConfig;
 	}
+	
 	
 	public LongTermArchivingClient() throws ServiceClientCreationException {
 		initArchivingSystemClient();

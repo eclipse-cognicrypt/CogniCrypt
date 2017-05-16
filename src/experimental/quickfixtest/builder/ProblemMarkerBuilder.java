14d5f5dca252f6a67839aae7325db5a0e4675865
Patrick Alexander Hill <pahill@mail.uni-paderborn.de>
2017-05-16 09:50:17 +0200

    clarified the issue, prints and the eclipse report - reformatted

--- src/experimental/quickfixtest/builder/ProblemMarkerBuilder.java
+++ src/experimental/quickfixtest/builder/ProblemMarkerBuilder.java
@@ -20,20 +20,25 @@ import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.MethodInvocation;
+
+import crossing.e1.configurator.Activator;
+
 import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.Expression;
 
 /**
- * Builder Class used to set a Marker to a part of a specific resource, that matches
- * the predefined pattern. This would probably be done by the analysis later, as it 
- * would find such an "error" and could then directly add such a Marker.
+ * Builder Class used to set a Marker to a part of a specific resource, that
+ * matches the predefined pattern. This would probably be done by the analysis
+ * later, as it would find such an "error" and could then directly add such a
+ * Marker.
  * 
  * Currently only works for the specific example "cypher.getinstance('AES')"
  * 
- * Can be enabled by right clicking the Project and then selecting Configure->Enable QFBuilder
- * For testing purposes consider disabling the auto build feature to initiate a manual full build
+ * Can be enabled by right clicking the Project and then selecting
+ * Configure->Enable QFBuilder For testing purposes consider disabling the auto
+ * build feature to initiate a manual full build
  * 
  * @author Patrick Hill
  */
@@ -46,19 +51,23 @@ public class ProblemMarkerBuilder extends IncrementalProjectBuilder {
 
 	// define a correct ID (get range of possible ones)
 	private static final int JDT_PROBLEM_ID = 10000000;
-	
+
 	private ASTParser parser;
 
 	/**
-	 * addMarker
-	 * Method that adds a Marker to a File, which can then be displayed as an
-	 * error/warning in the IDE.
+	 * addMarker Method that adds a Marker to a File, which can then be
+	 * displayed as an error/warning in the IDE.
 	 * 
-	 * @param file the IResource of the File to which the Marker is added
-	 * @param message the message the Marker is supposed to display
-	 * @param lineNumber the Line to which the Marker is supposed to be added
-	 * @param start the number of the start character for the Marker
-	 * @param end the number of the end character for the Marker
+	 * @param file
+	 *            the IResource of the File to which the Marker is added
+	 * @param message
+	 *            the message the Marker is supposed to display
+	 * @param lineNumber
+	 *            the Line to which the Marker is supposed to be added
+	 * @param start
+	 *            the number of the start character for the Marker
+	 * @param end
+	 *            the number of the end character for the Marker
 	 */
 	private void addMarker(IResource file, String message, int lineNumber, int start, int end) {
 		try {
@@ -71,17 +80,17 @@ public class ProblemMarkerBuilder extends IncrementalProjectBuilder {
 			marker.setAttribute(IMarker.LINE_NUMBER, lineNumber);
 			marker.setAttribute(IMarker.CHAR_START, start);
 			marker.setAttribute(IMarker.CHAR_END, end);
-			//IJavaModelMarker is important for the Quickfix Processor to work correctly
+			// IJavaModelMarker is important for the Quickfix Processor to work
+			// correctly
 			marker.setAttribute(IJavaModelMarker.ID, JDT_PROBLEM_ID);
 		} catch (CoreException e) {
 		}
 	}
-	
-	
+
 	/**
-	 * printProblemIDs
-	 * Method to print out all JDT Problem IDs which are taken by default.
-	 * Used to find a not taken, unique ID, which can be used for the specific Marker.
+	 * printProblemIDs Method to print out all JDT Problem IDs which are taken
+	 * by default. Used to find a not taken, unique ID, which can be used for
+	 * the specific Marker.
 	 */
 	public void printProblemIDs() {
 		Field[] fields = org.eclipse.jdt.core.compiler.IProblem.class.getFields();
@@ -90,27 +99,30 @@ public class ProblemMarkerBuilder extends IncrementalProjectBuilder {
 			try {
 				ints.add(field.getInt(null));
 			} catch (IllegalArgumentException e) {
-				e.printStackTrace();
+				Activator.getDefault().logError(e);
 			} catch (IllegalAccessException e) {
-				e.printStackTrace();
+				Activator.getDefault().logError(e);
 			}
 		}
 		Collections.sort(ints);
 		for (Integer integer : ints) {
+			// this prints the JDT Problem IDs if you have to pick a new one
+			// call this method.
 			System.out.println(integer);
 		}
 
 	}
-	
+
 	/**
-	 * startMarking
-	 * Method that starts the Marking process by getting the IPackageFragments
+	 * startMarking Method that starts the Marking process by getting the
+	 * IPackageFragments
 	 * 
-	 * @param project Needed for getting the PackageFragments
+	 * @param project
+	 *            Needed for getting the PackageFragments
 	 * @throws JavaModelException
 	 * @throws CoreException
 	 */
-	private void startMarking(IProject project) throws JavaModelException, CoreException{
+	private void startMarking(IProject project) throws JavaModelException, CoreException {
 		if (project.isNatureEnabled("org.eclipse.jdt.core.javanature")) {
 			IPackageFragment[] packages = JavaCore.create(project).getPackageFragments();
 			for (IPackageFragment mypackage : packages) {
@@ -118,29 +130,31 @@ public class ProblemMarkerBuilder extends IncrementalProjectBuilder {
 			}
 		}
 	}
-	
+
 	/**
-	 * getUnitForParser
-	 * Method to get the CompilationUnits from the PackageFragments and start the parsing process from there
+	 * getUnitForParser Method to get the CompilationUnits from the
+	 * PackageFragments and start the parsing process from there
 	 * 
-	 * @param mypackage IPackageFragment from startMarking
+	 * @param mypackage
+	 *            IPackageFragment from startMarking
 	 * @throws JavaModelException
 	 */
-	private void getUnitForParser(IPackageFragment mypackage) throws JavaModelException{
+	private void getUnitForParser(IPackageFragment mypackage) throws JavaModelException {
 		if (mypackage.getKind() == IPackageFragmentRoot.K_SOURCE) {
 			for (ICompilationUnit unit : mypackage.getCompilationUnits()) {
 				setupParser(unit);
 			}
 		}
 	}
-	
+
 	/**
-	 * setupParser
-	 * Method that sets up the Parser and creates a visitor for a specific unit
+	 * setupParser Method that sets up the Parser and creates a visitor for a
+	 * specific unit
 	 * 
-	 * @param unit Unit from getUnitForParser
+	 * @param unit
+	 *            Unit from getUnitForParser
 	 */
-	private void setupParser(ICompilationUnit unit){
+	private void setupParser(ICompilationUnit unit) {
 		ASTParser parser = ASTParser.newParser(AST.JLS8);
 		parser.setKind(ASTParser.K_COMPILATION_UNIT);
 		parser.setSource(unit);
@@ -148,14 +162,16 @@ public class ProblemMarkerBuilder extends IncrementalProjectBuilder {
 		CompilationUnit cu = (CompilationUnit) parser.createAST(null);
 		cu.accept(createMarkerVisitor(unit, cu));
 	}
-	
+
 	/**
-	 * createMarkerVisitor
-	 * Creates the Visitor that goes through the unit and sets the Marker based on 
-	 * a case, which is currently hard coded as cypher.getinstance('AES').
+	 * createMarkerVisitor Creates the Visitor that goes through the unit and
+	 * sets the Marker based on a case, which is currently hard coded as
+	 * cypher.getinstance('AES').
 	 * 
-	 * @param unit Unit from getUnitForParser
-	 * @param cu same as unit but different type
+	 * @param unit
+	 *            Unit from getUnitForParser
+	 * @param cu
+	 *            same as unit but different type
 	 * @return visitor for the unit
 	 */
 	private ASTVisitor createMarkerVisitor(ICompilationUnit unit, CompilationUnit cu) {
@@ -177,12 +193,10 @@ public class ProblemMarkerBuilder extends IncrementalProjectBuilder {
 		};
 		return visitor;
 	}
-	
 
 	/**
-	 * build
-	 * The standard method that gets called by eclipse when initiating a build for its 
-	 * specified Nature 
+	 * build The standard method that gets called by eclipse when initiating a
+	 * build for its specified Nature
 	 */
 	protected IProject[] build(int kind, Map args, IProgressMonitor monitor) throws CoreException {
 		clean(null);
@@ -192,14 +206,14 @@ public class ProblemMarkerBuilder extends IncrementalProjectBuilder {
 		try {
 			startMarking(project);
 		} catch (CoreException e) {
-			e.printStackTrace();
+			Activator.getDefault().logError(e);
 		}
 		return null;
 	}
 
 	/**
-	 * clean
-	 * Method to clean a file of its Markers. Needs to be done before adding new ones
+	 * clean Method to clean a file of its Markers. Needs to be done before
+	 * adding new ones
 	 */
 	protected void clean(IProgressMonitor monitor) throws CoreException {
 		getProject().deleteMarkers(MARKER_TYPE, true, IResource.DEPTH_INFINITE);
--- src/experimental/quickfixtest/quickfix/QuickFixProcessor.java
+++ src/experimental/quickfixtest/quickfix/QuickFixProcessor.java
@@ -13,11 +13,14 @@ import org.eclipse.jface.text.contentassist.IContextInformation;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
 
+import crossing.e1.configurator.Activator;
+
 /**
- * Quickfix Processor that checks for the specific ProblemID of the Marker 
- * and then gets the IJavaCompletionProposal for it.
+ * Quickfix Processor that checks for the specific ProblemID of the Marker and
+ * then gets the IJavaCompletionProposal for it.
  * 
- * The completion proposal is the thing that could be seen as the actual Quickfix.
+ * The completion proposal is the thing that could be seen as the actual
+ * Quickfix.
  * 
  * Gets invoked automatically by Eclipse once a Java Problem is found.
  * 
@@ -25,96 +28,98 @@ import org.eclipse.swt.graphics.Point;
  *
  */
 public class QuickFixProcessor implements IQuickFixProcessor {
-	
-	private final static int JDT_PROBLEM_ID = 10000000; //get correct one
+
+	private final static int JDT_PROBLEM_ID = 10000000; // get correct one
 	private static final String MARKER_TYPE = "QuickFixTest.OCCEProblem";
 
 	/**
-	 * getCorrections
-	 * Method that gets invoked if hasCorrections returns true
+	 * getCorrections Method that gets invoked if hasCorrections returns true
 	 * Returns a new completion proposal for the specific problem
 	 */
 	public IJavaCompletionProposal[] getCorrections(IInvocationContext context, IProblemLocation[] locations)
 			throws CoreException {
 		if (locations == null || locations.length == 0) {
 			/*
-			 * https://bugs.eclipse.org/444120 Eclipse can call this method any 
-			 * locations, if a quick fix is requested without any problems.
+			 * https://bugs.eclipse.org/444120 Eclipse can call this method even
+			 * when there are no markers, then this case would occur.
 			 */
 			return null;
 		}
-		
 
 		return new IJavaCompletionProposal[] { new IJavaCompletionProposal() {
 
-		/**
-		 * apply
-		 * Method that applys the Quickfix/CompletionProposal
-		 * Currently replaces the text of the example case "cypher.getinstance('AES')"
-		 * with "cypher.getinstance('AES/CBC/PKCS5PADDING')"
-		 * 
-		 * @param d Document or File for which the Quickfix is supposed to work
-		 */
-		public void apply(IDocument d) {
+			/**
+			 * apply Method that applys the Quickfix/CompletionProposal
+			 * Currently replaces the text of the example case
+			 * "cypher.getinstance('AES')" with
+			 * "cypher.getinstance('AES/CBC/PKCS5PADDING')"
+			 * 
+			 * @param d
+			 *            Document or File for which the Quickfix is supposed to
+			 *            work
+			 */
+			public void apply(IDocument d) {
 				try {
-					d.replace(context.getSelectionOffset(), context.getSelectionLength(), "Cipher.getInstance(\"AES/CBC/PKCS5PADDING\")");
+					d.replace(context.getSelectionOffset(), context.getSelectionLength(),
+							"Cipher.getInstance(\"AES/CBC/PKCS5PADDING\")");
 					context.getCompilationUnit().getResource().deleteMarkers(MARKER_TYPE, false, IResource.DEPTH_ZERO);
 				} catch (CoreException e) {
-					// TODO Auto-generated catch block
+					Activator.getDefault().logError(e);
 					e.printStackTrace();
 				} catch (BadLocationException e) {
-					// TODO Auto-generated catch block
+					Activator.getDefault().logError(e);
 					e.printStackTrace();
 				}
-		}
+			}
 
-		/**
-		 * Not used in the example
-		 */
-		public String getAdditionalProposalInfo() {
-			return null;
-		}
+			/**
+			 * Not used in the example
+			 */
+			public String getAdditionalProposalInfo() {
+				return null;
+			}
 
-		/**
-		 * Not used in the example
-		 */
-		public IContextInformation getContextInformation() {
-			return null;
-		}
+			/**
+			 * Not used in the example
+			 */
+			public IContextInformation getContextInformation() {
+				return null;
+			}
 
-		/**
-		 * Display String for the Quickfix
-		 */
-		public String getDisplayString() {
-			String display = "Change to a more secure Encryption";
-			return display;
-		}
+			/**
+			 * Display String for the Quickfix
+			 */
+			public String getDisplayString() {
+				String display = "Change to a more secure Encryption";
+				return display;
+			}
 
-		/**
-		 * Not used in the example
-		 */
-		public Image getImage() {
-			return null;
-		}
+			/**
+			 * Not used in the example
+			 */
+			public Image getImage() {
+				return null;
+			}
 
-		/**
-		 * Not used in the example
-		 */
-		public Point getSelection(IDocument document) {
-			return null;
-		}
+			/**
+			 * Not used in the example
+			 */
+			public Point getSelection(IDocument document) {
+				return null;
+			}
 
-		/**
-		 * Used to define the relevance of a Quickfix
-		 */
-		public int getRelevance() {
-			return 1;
-		}}};
+			/**
+			 * Used to define the relevance of a Quickfix
+			 */
+			public int getRelevance() {
+				return 1;
+			}
+		} };
 	}
 
 	/**
-	 * hasCorrections
-	 * checks if the Problem is supposed to be solved by this Quickfix Processor
+	 * hasCorrections checks if the Problem is supposed to be solved by this
+	 * Quickfix Processor
 	 */
 	public boolean hasCorrections(ICompilationUnit unit, int problemId) {
 		return problemId == JDT_PROBLEM_ID;

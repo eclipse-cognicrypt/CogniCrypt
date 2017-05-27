d2903f783a97802884752bc2be8ba045551eea0d
Stefan Krueger <stefan.krueger@crossing.tu-darmstadt.de>
2017-05-27 19:26:30 +0200

    Worked more on Analysis integration with CogniCrypt.
    Removed CryptSL-related classes to ideal. Extended CryptSLModelReader.
    
    Former-commit-id: de3a08bee3ee543ce6d9e86026f04209f688c853

---
+++ src/main/java/crossing/e1/configurator/Analysis/CryptoAnalysis.java
@@ -0,0 +1,16 @@
+package crossing.e1.configurator.Analysis;
+
+import java.util.List;
+
+import crypto.rules.CryptSLRule;
+
+
+public class CryptoAnalysis {
+	
+	List<CryptSLRule> specs;
+	
+	public CryptoAnalysis(List<CryptSLRule> rules) {
+		specs = rules;
+	}
+
+}

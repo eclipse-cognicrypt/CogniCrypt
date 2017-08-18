889b37e84e1c1fd53556a01c9e9cdde1aaa0bf6a
Ahmed Ben Tahar <ahmed_bentaher@ymail.com>
2017-08-18 15:49:15 +0200

    creation of wizard class + creation of JSON file for the questions

---
+++ src/main/java/crossing/e1/configurator/wizard/IntegrationNewPrimitive.java
@@ -0,0 +1,14 @@
+package crossing.e1.configurator.wizard;
+
+import org.eclipse.jface.wizard.Wizard;
+
+
+public class IntegrationNewPrimitive extends Wizard {
+
+	@Override
+	public boolean performFinish() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}

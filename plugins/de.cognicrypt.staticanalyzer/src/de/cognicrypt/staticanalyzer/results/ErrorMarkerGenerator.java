cdf3d5bb90b1c16dcce1275571623a34d8375d10
Stefan Krueger <stefan.krueger@crossing.tu-darmstadt.de>
2017-10-16 15:52:01 +0200

    Worked more on #98.3 was done in previous commit by @johspaeth.
    2) Done except for the descriptions. Reports related to constraint
    violations not working b/c constraint solver does not provide unit as of
    now. Updated CryptoAnalysis jar.
    
    Former-commit-id: dcd30ad9fb81e38fe77c381c8093d9a2568d0b43

---
+++ plugins/de.cognicrypt.staticanalyzer/src/de/cognicrypt/staticanalyzer/results/ErrorMarkerGenerator.java
@@ -0,0 +1,53 @@
+package de.cognicrypt.staticanalyzer.results;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+
+import de.cognicrypt.staticanalyzer.Activator;
+
+public class ErrorMarkerGenerator {
+
+	private List<IMarker> markers;
+	
+	public ErrorMarkerGenerator() {
+		markers = new ArrayList<IMarker>();
+	}
+	
+	public boolean addMarker(IResource res, int line, String message) {
+		if (!res.exists() || !res.isAccessible()) {
+			Activator.getDefault().logError("No resource to generate error marker for found.");
+			return false;
+		}
+		IMarker marker;
+		try {
+			marker = res.createMarker(IMarker.PROBLEM);
+			marker.setAttribute(IMarker.LINE_NUMBER, line);
+			marker.setAttribute(IMarker.MESSAGE, message);
+			marker.setAttribute(IMarker.PRIORITY, IMarker.PRIORITY_HIGH);
+			marker.setAttribute(IMarker.SEVERITY, IMarker.SEVERITY_ERROR);
+		} catch (CoreException e) {
+			Activator.getDefault().logError(e);
+			return false;
+		}
+		markers.add(marker);
+		return true;
+	}
+	
+	public Boolean clearMarkers() {
+		for (IMarker marker : markers) {
+			try {
+				marker.delete();
+			} catch (CoreException e) {
+				Activator.getDefault().logError(e);
+				return false;
+			}
+		}
+		markers.clear();
+		return true;
+	}
+	
+}

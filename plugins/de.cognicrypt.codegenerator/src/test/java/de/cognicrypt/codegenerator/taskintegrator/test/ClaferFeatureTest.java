b6bbbfb6529d6f21c296dcc53bdcf70216933f1f
André Birke <abirke@campus.uni-paderborn.de>
2017-11-21 16:22:42 +0100

    implement unit test for ClaferFeature.toString

--- plugins/de.cognicrypt.codegenerator/src/test/java/de/cognicrypt/codegenerator/taskintegrator/test/ClaferFeatureTest.java
+++ plugins/de.cognicrypt.codegenerator/src/test/java/de/cognicrypt/codegenerator/taskintegrator/test/ClaferFeatureTest.java
@@ -3,7 +3,9 @@ package de.cognicrypt.codegenerator.taskintegrator.test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -17,6 +19,8 @@ import de.cognicrypt.codegenerator.taskintegrator.models.ClaferFeature;
 import de.cognicrypt.codegenerator.taskintegrator.models.FeatureProperty;
 
 public class ClaferFeatureTest {
+
+	public static final String testFileFolder = "src/test/resources/taskintegrator/";
 
 	@Test
 	public final void testPropertyAmount() {
@@ -38,6 +42,31 @@ public class ClaferFeatureTest {
 		byte[] actualBytes = Files.readAllBytes(actualFilePath);
 		
 		return Arrays.equals(expectedBytes, actualBytes);
+	}
+
+	@Test
+	public final void testClaferFeatureToString() throws IOException {
+		// file to test against
+		String expectedFilename = testFileFolder + "testFile1.cfr";
+
+		// programmatically created Clafer feature
+		ClaferFeature cfrFeature = new ClaferFeature(Constants.FeatureType.ABSTRACT, "Algorithm", "");
+		cfrFeature.getfeatureProperties().add(new FeatureProperty("securityLevel", "Security"));
+
+		// generate file from ClaferFeature instance
+		String actualFilename = testFileFolder + "testFile1_tmp.cfr";
+		Path actualPath = Paths.get(actualFilename);
+
+		Charset charset = Charset.forName("UTF-8");
+		String s = cfrFeature.toString();
+		try (BufferedWriter writer = Files.newBufferedWriter(actualPath, charset)) {
+			writer.write(s, 0, s.length());
+		} catch (IOException x) {
+			System.err.format("IOException: %s%n", x);
+		}
+
+		// compare the files
+		assertTrue(filesEqual(expectedFilename, actualFilename));
 	}
 
 }

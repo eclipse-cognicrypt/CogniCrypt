bd12cef0707fc487beeb33d7926420975472db06
André Birke <abirke@campus.uni-paderborn.de>
2017-11-27 10:31:15 +0100

    create test class for task integration wizard
    Former-commit-id: 390e4e35d2aa4da3c0b2da500de151341041fde8

---
+++ plugins/de.cognicrypt.codegenerator/src/test/java/de/cognicrypt/codegenerator/taskintegrator/test/TaskIntegrationWizardTest.java
@@ -0,0 +1,15 @@
+package de.cognicrypt.codegenerator.taskintegrator.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class TaskIntegrationWizardTest {
+
+	@Test
+	public void testGetTIPageByName() {
+		fail("Not yet implemented");
+	}
+
+}

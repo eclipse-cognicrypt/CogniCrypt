c084c69604789ece9cc1189a63f2ce7cdf6c57d6
anan <anan@mail.uni-paderborn.de>
2017-11-27 11:06:35 +0100

    changing combo box to slider
    Former-commit-id: 39eeea1b10bac569e2d778699dc3a21d7055e8fb

--- plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/Constants.java
+++ plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/Constants.java
@@ -27,7 +27,7 @@ package de.cognicrypt.codegenerator;
 public class Constants {
 
 	public static enum GUIElements {
-		combo, text, itemselection,button,radio
+		combo, text, itemselection,button,radio, scale
 	}
 
 	//The plugin is bundled in a jar archive and the file separator within jar files is / (see: https://stackoverflow.com/questions/24749007/how-to-use-file-separator-for-a-jar-file-resource).

afe85554815b4ee40dbfe736a73b5341b8a60235
Ravi Shaw <ravishaw6191@gmail.com>
2017-12-30 15:33:53 +0100

    Overridden the behavior of finish button on pageForLinkAnswer
    To check the creation of the json file  
    
    Former-commit-id: 641c0b62aaf4c545c53e2b88fa4087023701332e

--- plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/taskintegrator/wizard/TaskIntegrationWizard.java
+++ plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/taskintegrator/wizard/TaskIntegrationWizard.java
@@ -1,11 +1,15 @@
 package de.cognicrypt.codegenerator.taskintegrator.wizard;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 
 import de.cognicrypt.codegenerator.Constants;
+import de.cognicrypt.codegenerator.question.Question;
 import de.cognicrypt.codegenerator.taskintegrator.controllers.FileUtilities;
 import de.cognicrypt.codegenerator.taskintegrator.models.ModelAdvancedMode;
 import de.cognicrypt.codegenerator.taskintegrator.widgets.CompositeToHoldGranularUIElements;
@@ -68,11 +72,30 @@ public class TaskIntegrationWizard extends Wizard {
 		if(this.getContainer().getCurrentPage().getName().equals(Constants.PAGE_NAME_FOR_MODE_OF_WIZARD)){
 			if(objectForDataInNonGuidedMode.isGuidedModeChosen() == false //&& this.objectForDataInNonGuidedMode.isGuidedModeForced() == false
 				){
-				fileUtilities.writeFiles(objectForDataInNonGuidedMode.getLocationOfClaferFile(), objectForDataInNonGuidedMode.getLocationOfJSONFile(), objectForDataInNonGuidedMode.getLocationOfXSLFile(), objectForDataInNonGuidedMode.getLocationOfCustomLibrary());			
+				fileUtilities.writeFiles(objectForDataInNonGuidedMode.getLocationOfClaferFile(), objectForDataInNonGuidedMode.getLocationOfJSONFile(), objectForDataInNonGuidedMode.getLocationOfXSLFile(),null);			
 				return true;
 			}
 		}
-
+		
+		/*
+		 * for retrieving the details of questions
+		 */
+		if(this.getContainer().getCurrentPage().getName().equals(Constants.PAGE_NAME_FOR_LINK_ANSWERS)){
+		PageForTaskIntegratorWizard linkAnsPage=(PageForTaskIntegratorWizard)getTIPageByName(Constants.PAGE_NAME_FOR_LINK_ANSWERS);
+		CompositeToHoldGranularUIElements linkAnsPageComposite=(CompositeToHoldGranularUIElements)linkAnsPage.getCompositeToHoldGranularUIElements();
+		ArrayList<Question> listOfAllQuestions = linkAnsPageComposite.getListOfAllQuestions();
+		System.out.println(listOfAllQuestions.size());
+		FileUtilities writeJsonFile = new FileUtilities(objectForDataInNonGuidedMode.getNameOfTheTask());
+		try {
+			writeJsonFile.writeFiles(null, listOfAllQuestions, null, null);
+			//writeJsonFile.writeJSONFile(listOfAllQuestions);
+		}/* catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/finally{
+			return true;
+		}
+		}
 		return false;
 	}
 

043c32e051a82dccf19346f542a284d7d5b10cbe
André Birke <abirke@campus.uni-paderborn.de>
2018-01-18 13:51:25 +0100

    remove todos from FeaturePropertiesLabelProvider

--- plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/taskintegrator/widgets/FeaturePropertiesLabelProvider.java
+++ plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/taskintegrator/widgets/FeaturePropertiesLabelProvider.java
@@ -11,31 +11,23 @@ public class FeaturePropertiesLabelProvider implements ILabelProvider {
 
 	@Override
 	public void addListener(ILabelProviderListener arg0) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public boolean isLabelProperty(Object arg0, String arg1) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public void removeListener(ILabelProviderListener arg0) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public Image getImage(Object arg0) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 

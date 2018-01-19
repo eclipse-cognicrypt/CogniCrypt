6271b70f1ac8e53070a42c7e32eb9f92feba5a35
André Birke <abirke@campus.uni-paderborn.de>
2018-01-19 16:52:56 +0100

    make ClaferFeatureDialog positioning dynamic

--- plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/taskintegrator/widgets/CompositeToHoldSmallerUIElements.java
+++ plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/taskintegrator/widgets/CompositeToHoldSmallerUIElements.java
@@ -6,8 +6,6 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.layout.RowData;
-import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;

24c4e9823a8ba881138b04733b05a05eebed3117
André Birke <abirke@campus.uni-paderborn.de>
2018-01-22 11:15:45 +0100

    add ClaferModelTest class
    Former-commit-id: a9248d147560d74f3b5ef971e1173a9f05ff0e4a

---
+++ plugins/de.cognicrypt.codegenerator/src/test/java/de/cognicrypt/codegenerator/taskintegrator/test/ClaferModelTest.java
@@ -0,0 +1,15 @@
+package de.cognicrypt.codegenerator.taskintegrator.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class ClaferModelTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

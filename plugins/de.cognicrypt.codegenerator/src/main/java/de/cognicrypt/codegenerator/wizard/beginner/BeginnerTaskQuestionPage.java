8af1745e2da5a8b7243299be833b79014bd396cf
Sushmitha Sekar <sushmithasekar28@gmail.com>
2018-01-25 11:30:20 +0100

    CheckBox modified
    Former-commit-id: 3a371b1d082532a86000ba3896bdc2786092044c

--- plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/wizard/beginner/BeginnerTaskQuestionPage.java
+++ plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/wizard/beginner/BeginnerTaskQuestionPage.java
@@ -365,10 +365,11 @@ public class BeginnerTaskQuestionPage extends WizardPage {
 				checkBox.addSelectionListener(new SelectionAdapter() {
 					@Override
 					public void widgetSelected(SelectionEvent e) {
-						//if(checkBox.getSelection() == true)
-						inputField.setEchoChar( '\0' );
-						//getWizard().getContainer().updateButtons();
-					
+						if(checkBox.getSelection() == true){
+							inputField.setEchoChar( '\0' );
+						} else {	
+							inputField.setEchoChar((char) 0x25cf);
+						}					
 					}
 				});
 				text(question, inputField);

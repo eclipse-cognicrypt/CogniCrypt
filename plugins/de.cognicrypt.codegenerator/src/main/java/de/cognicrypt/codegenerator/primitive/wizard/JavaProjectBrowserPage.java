b29239c8da318fa210e2802fba7ba0adb335a9d4
Unknown <ahmed_bentaher@ymail.com>
2018-01-27 22:49:38 +0100

    add class description

--- plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/primitive/wizard/JavaProjectBrowserPage.java
+++ plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/primitive/wizard/JavaProjectBrowserPage.java
@@ -1,6 +1,7 @@
 package de.cognicrypt.codegenerator.primitive.wizard;
 
 import java.io.File;
+import java.lang.reflect.Method;
 
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
@@ -14,8 +15,9 @@ import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
-/*
- * author: Ahmed Ben Tahar
+/**
+ * 
+ * @author Ahmed Ben Tahar
  */
 public class JavaProjectBrowserPage extends WizardPage {
 
@@ -31,7 +33,6 @@ public class JavaProjectBrowserPage extends WizardPage {
 	public void createControl(Composite parent) {
 		Composite container = new Composite(parent, SWT.NULL);
 		setControl(container);
-		//		container.getShell().setSize(500, 300);
 		container.setBounds(10, 10, 200, 300);
 
 		container.setLayout(new GridLayout(2, true));
@@ -40,35 +41,37 @@ public class JavaProjectBrowserPage extends WizardPage {
 		Label question = new Label(container, SWT.NULL);
 		question.setText("Please select your Java Project from your local");
 		new Label(container, SWT.NONE);
+		new Label(container, SWT.NONE);
+				new Label(container, SWT.NONE);
+		
+				text = new Text(container, SWT.BORDER | SWT.READ_ONLY);
+				GridData gd_text = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+				gd_text.widthHint = 242;
+				text.setLayoutData(gd_text);
+		
+				Button btnBrowse = new Button(container, SWT.PUSH);
+				GridData gd_btnBrowse = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+				gd_btnBrowse.widthHint = 85;
+				btnBrowse.setLayoutData(gd_btnBrowse);
+				btnBrowse.setBounds(295, 140, 75, 25);
+				btnBrowse.setText("Browse");
+				btnBrowse.addSelectionListener(new SelectionAdapter() {
 
-		text = new Text(container, SWT.BORDER | SWT.READ_ONLY);
-		GridData gd_text = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
-		gd_text.widthHint = 242;
-		text.setLayoutData(gd_text);
+					@Override
+					public void widgetSelected(SelectionEvent e) {
+						FileDialog dialog = new FileDialog(btnBrowse.getShell(), SWT.NULL);
+						String path = dialog.open();
+						if (path != null) {
 
-		Button btnBrowse = new Button(container, SWT.PUSH);
-		GridData gd_btnBrowse = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
-		gd_btnBrowse.widthHint = 85;
-		btnBrowse.setLayoutData(gd_btnBrowse);
-		btnBrowse.setBounds(295, 140, 75, 25);
-		btnBrowse.setText("Browse");
-		btnBrowse.addSelectionListener(new SelectionAdapter() {
+							File file = new File(path);
+							if (file.isFile())
+								displayFiles(new String[] { file.toString() });
+							else
+								displayFiles(file.list());
+						}
+					}
 
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				FileDialog dialog = new FileDialog(btnBrowse.getShell(), SWT.NULL); //getParent.getShel()
-				String path = dialog.open();
-				if (path != null) {
-
-					File file = new File(path);
-					if (file.isFile())
-						displayFiles(new String[] { file.toString() });
-					else
-						displayFiles(file.list());
-				}
-			}
-
-		});
+				});
 	}
 
 	public void displayFiles(String[] files) {
@@ -78,4 +81,12 @@ public class JavaProjectBrowserPage extends WizardPage {
 
 		}
 	}
+
+	// Test if the file selected is Java project or not
+	public boolean isJavaProject(File file) {
+    
+		return true;
+	}
+	
+
 }

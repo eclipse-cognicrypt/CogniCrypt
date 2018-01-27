a67eb8e0e9830af6157000b1e257339cb6b20a55
Rajiv Thorat <rajivthorat2003@gmail.com>
2018-01-27 16:00:14 +0100

    Wrong file added to the tasks for the model
    Former-commit-id: b87842b26f25f37b3f9bf2cc5a9a1e0f81c08ea0

--- plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/taskintegrator/models/ModelAdvancedMode.java
+++ plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/taskintegrator/models/ModelAdvancedMode.java
@@ -160,7 +160,7 @@ public class ModelAdvancedMode {
 	public void setTask() {
 		task.setName(getNameOfTheTask());
 		task.setDescription(getDescription());
-		task.setModelFile(Constants.CFR_FILE_DIRECTORY_PATH + getNameOfTheTask() + Constants.CFR_EXTENSION);
+		task.setModelFile(Constants.CFR_FILE_DIRECTORY_PATH + getNameOfTheTask() + Constants.JS_EXTENSION);
 		task.setQuestionsJSONFile(Constants.JSON_FILE_DIRECTORY_PATH + getNameOfTheTask() + Constants.JSON_EXTENSION);
 		task.setTaskDescription(getTaskDescription());
 		task.setXslFile(Constants.XSL_FILE_DIRECTORY_PATH + getNameOfTheTask() + Constants.XSL_EXTENSION);

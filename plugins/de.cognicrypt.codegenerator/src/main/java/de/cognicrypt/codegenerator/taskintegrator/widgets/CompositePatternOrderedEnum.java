5a67a2fdc9fffc14f7bd1230dfb7705e5c55b738
André Birke <abirke@campus.uni-paderborn.de>
2018-02-19 13:21:52 +0100

    add pattern Ordered Enumeration

---
+++ plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/taskintegrator/widgets/CompositePatternOrderedEnum.java
@@ -0,0 +1,119 @@
+package de.cognicrypt.codegenerator.taskintegrator.widgets;
+
+import java.util.ArrayList;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.ScrolledComposite;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+
+public class CompositePatternOrderedEnum extends Composite {
+
+	private Composite compositeOptions;
+	private ScrolledComposite compositeScrolledOptions;
+	
+	private ArrayList<CompositeSortableTextItem> sortableTextItems;
+
+	private ArrayList<StringBuilder> options;
+
+	public CompositePatternOrderedEnum(Composite parent) {
+		super(parent, SWT.NONE);
+		
+		options = new ArrayList<>();
+		sortableTextItems = new ArrayList<>();
+
+		setLayout(new GridLayout(1, false));
+
+		Button btnAddOption = new Button(this, SWT.NONE);
+		btnAddOption.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false));
+		btnAddOption.setText("Add option");
+		btnAddOption.addSelectionListener(new SelectionAdapter() {
+
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				StringBuilder strOption = new StringBuilder();
+				options.add(strOption);
+
+				CompositeSortableTextItem compositeSortableTextItem = new CompositeSortableTextItem(compositeOptions);
+				compositeSortableTextItem.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+
+				sortableTextItems.add(compositeSortableTextItem);
+
+				compositeOptions.layout();
+				compositeScrolledOptions.setMinSize(compositeOptions.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+				super.widgetSelected(e);
+			}
+		});
+
+		compositeScrolledOptions = new ScrolledComposite(this, SWT.BORDER | SWT.V_SCROLL);
+		compositeScrolledOptions.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
+		compositeScrolledOptions.setLayout(new GridLayout(1, false));
+
+		compositeOptions = new Composite(compositeScrolledOptions, SWT.NONE);
+		compositeOptions.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		compositeOptions.setLayout(new GridLayout(1, false));
+		compositeScrolledOptions.setContent(compositeOptions);
+
+		compositeScrolledOptions.setExpandHorizontal(true);
+		compositeScrolledOptions.setExpandVertical(true);
+		compositeScrolledOptions.setMinSize(compositeOptions.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+	}
+
+	private int getItemPosition(CompositeSortableTextItem needleItem) {
+		int index = -1;
+		
+		for (int i=0; i < sortableTextItems.size(); i++) {
+			CompositeSortableTextItem refComposite = sortableTextItems.get(i);
+			if (refComposite == needleItem) {
+				index = i;
+			}
+		}
+		
+		return index;
+	}
+
+	private void swapTexts(int i, int j) {
+		String tempString = sortableTextItems.get(i).getText();
+		sortableTextItems.get(i).setText(sortableTextItems.get(j).getText());
+		sortableTextItems.get(j).setText(tempString);
+	}
+
+	public void moveUp(CompositeSortableTextItem targetComposite) {
+
+		int targetIndex = getItemPosition(targetComposite);
+
+		if (targetIndex == -1 || targetIndex == 0) {
+			return;
+		}
+		
+		swapTexts(targetIndex, targetIndex - 1);
+	}
+
+	public void moveDown(CompositeSortableTextItem targetComposite) {
+
+		int targetIndex = getItemPosition(targetComposite);
+
+		if (targetIndex == -1 || targetIndex == sortableTextItems.size() - 1) {
+			return;
+		}
+
+		swapTexts(targetIndex, targetIndex + 1);
+	}
+
+	public void remove(CompositeSortableTextItem targetComposite) {
+		sortableTextItems.remove(targetComposite);
+		targetComposite.dispose();
+
+		compositeOptions.layout();
+		compositeScrolledOptions.setMinSize(compositeOptions.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+	}
+
+	public ArrayList<StringBuilder> getElements() {
+		return options;
+	}
+
+}

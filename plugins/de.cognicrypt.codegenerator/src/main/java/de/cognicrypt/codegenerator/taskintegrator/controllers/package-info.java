b8372bba480ebe98c4ce1f186f12c870f8600586
anan <anan@mail.uni-paderborn.de>
2018-02-22 16:48:16 +0100

    temp commit(removed ti packages)

--- plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/taskintegrator/controllers/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Control and validation of the CogniCrypt task integration
- * 
- * @author sics1718
- */
-package de.cognicrypt.codegenerator.taskintegrator.controllers;
--- plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/taskintegrator/widgets/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * UI elements that represent questions and answers, Clafer features and so on
- *
- * @author sics1718
- */
-package de.cognicrypt.codegenerator.taskintegrator.widgets;

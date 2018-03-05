57cd227f6054e57f557ca766d6e12f3148b10694
Stefan Krueger <stefan.krueger@crossing.tu-darmstadt.de>
2018-03-05 14:29:57 +0100

    Error msgs for broken value constraints now display var name from code.
    Several minor fixes for other error messages.

--- plugins/de.cognicrypt.staticanalyzer/src/de/cognicrypt/staticanalyzer/Constants.java
+++ plugins/de.cognicrypt.staticanalyzer/src/de/cognicrypt/staticanalyzer/Constants.java
@@ -3,5 +3,7 @@ package de.cognicrypt.staticanalyzer;
 public class Constants {
 
 	public static final String NO_RES_FOUND = "No resource to generate error marker for found.";
+	public static final String OBJECT_OF_TYPE = "object of type ";
+	public static final String VAR = "variable ";
 
 }

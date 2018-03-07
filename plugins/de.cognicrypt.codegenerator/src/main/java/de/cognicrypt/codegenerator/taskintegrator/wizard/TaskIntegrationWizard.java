4928b35d1b0312a25178ef9c04d746cceefc1a86
André Birke <abirke@campus.uni-paderborn.de>
2018-03-07 17:35:00 +0100

    remove empty getNextPage method in TaskIntegrationWizard

--- plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/taskintegrator/wizard/TaskIntegrationWizard.java
+++ plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/taskintegrator/wizard/TaskIntegrationWizard.java
@@ -5,8 +5,6 @@ import java.util.ArrayList;
 
 import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jface.wizard.Wizard;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
 
 import de.cognicrypt.codegenerator.Constants;
 import de.cognicrypt.codegenerator.question.Question;
@@ -16,9 +14,8 @@ import de.cognicrypt.codegenerator.taskintegrator.models.ModelAdvancedMode;
 import de.cognicrypt.codegenerator.taskintegrator.widgets.CompositeForXsl;
 import de.cognicrypt.codegenerator.taskintegrator.widgets.CompositeToHoldGranularUIElements;
 
-
-
 public class TaskIntegrationWizard extends Wizard {
+
 	public TaskIntegrationWizard() {
 
 	}
@@ -108,42 +105,6 @@ public class TaskIntegrationWizard extends Wizard {
 		}
 		}
 		return false;
-	}
-
-
-	/* (non-Javadoc)
-	 * @see org.eclipse.jface.wizard.Wizard#getNextPage(org.eclipse.jface.wizard.IWizardPage)
-	 */
-	@Override
-	public IWizardPage getNextPage(IWizardPage page) {
-
-		/*
-		 * TODO next button behavior important for the mode selection page. Postponing this code to
-		 * a future time, but within the Sept milestone.
-		switch(page.getName()){
-			case Constants.PAGE_NAME_FOR_MODE_OF_WIZARD:
-				if(((PageForTaskIntegratorWizard) this.getPage(Constants.PAGE_NAME_FOR_MODE_OF_WIZARD)).getCompositeChoiceForModeOfWizard() != null){
-
-				}
-				break;
-			case Constants.PAGE_NAME_FOR_CLAFER_FILE_CREATION:
-				break;
-			case Constants.PAGE_NAME_FOR_XSL_FILE_CREATION:
-				break;
-			case Constants.PAGE_NAME_FOR_HIGH_LEVEL_QUESTIONS:
-				break;
-		}
-			*/
-		// TODO for debuggnig only.
-		if(page.getName().equals(Constants.PAGE_NAME_FOR_CLAFER_FILE_CREATION)){
-			PageForTaskIntegratorWizard claferPage = (PageForTaskIntegratorWizard) page;
-			CompositeToHoldGranularUIElements compoGran = (CompositeToHoldGranularUIElements) claferPage.getCompositeToHoldGranularUIElements();
-			Control granComp = compoGran.getChildren()[0];
-			Control granContro[] = ((Composite) granComp).getChildren();
-			System.out.println("");
-		}
-		
-		return super.getNextPage(page);
 	}
 
 	/**

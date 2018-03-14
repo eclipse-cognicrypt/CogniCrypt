9eafd9c5ce3f452cd367045bc5e372ec7380d0c0
Ravi Shaw <ravishaw6191@gmail.com>
2018-03-14 21:58:21 +0100

    Class created to read the questions and pages from a .json file
    present QuestionJSONReader class doesn't seems to correct when printing
    the receive data
    
    Former-commit-id: 2fe9c85b2dd7ca7a6a2d65e63df0eafe6e49a121

---
+++ plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/question/QuestionsJSONReaderTI.java
@@ -0,0 +1,53 @@
+package de.cognicrypt.codegenerator.question;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import de.cognicrypt.codegenerator.Activator;
+import de.cognicrypt.codegenerator.utilities.Utils;
+
+public class QuestionsJSONReaderTI {
+
+	/**
+	 * 
+	 * @param filePath
+	 *        the path of the file
+	 * @return list of all questions contained in the file
+	 */
+	public ArrayList<Question> readQuestionsFromFile(String filePath) {
+		ArrayList<Question> originalQuestionList = new ArrayList<>();
+		try {
+			final BufferedReader reader = new BufferedReader(new FileReader(Utils.getResourceFromWithin(filePath)));
+			Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
+			originalQuestionList = gson.fromJson(reader, new TypeToken<ArrayList<Question>>() {}.getType());
+		} catch (FileNotFoundException e) {
+			Activator.getDefault().logError(e);
+		}
+		return originalQuestionList;
+	}
+
+	/**
+	 * 
+	 * @param filePath
+	 *        the path of the file
+	 * @return list of all pages contained in the file
+	 */
+	public ArrayList<Page> readPageFromFile(String filePath) {
+		ArrayList<Page> originalPageList = new ArrayList<>();
+		try {
+			final BufferedReader reader = new BufferedReader(new FileReader(Utils.getResourceFromWithin(filePath)));
+			Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
+			originalPageList = gson.fromJson(reader, new TypeToken<ArrayList<Page>>() {}.getType());
+		} catch (FileNotFoundException e) {
+			Activator.getDefault().logError(e);
+		}
+		return originalPageList;
+	}
+
+}

418b3956c97fd991c8edb1730da635645b7a620c
André Birke <abirke@campus.uni-paderborn.de>
2018-03-14 15:24:04 +0100

    increase coverage of ClaferModelContentProvider
    This commits adds testing of filters in the
    content provider to achieve full coverage of
    the class ClaferModelContentProvider.

--- plugins/de.cognicrypt.codegenerator/src/test/java/de/cognicrypt/codegenerator/taskintegrator/test/ClaferModelContentProviderTest.java
+++ plugins/de.cognicrypt.codegenerator/src/test/java/de/cognicrypt/codegenerator/taskintegrator/test/ClaferModelContentProviderTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -19,10 +20,17 @@ public class ClaferModelContentProviderTest {
 
 	ClaferModel claferModel;
 	ClaferModelContentProvider contentProvider;
+	ClaferModelContentProvider contentProviderWithFilters;
 
 	@Before
 	public final void createContentProvider() {
 		contentProvider = new ClaferModelContentProvider();
+
+		// create a content provider with filters attached
+		// - feature filter: only inheriting features
+		// - property filter: only primitive properties
+		contentProviderWithFilters = new ClaferModelContentProvider(feature -> !feature.getFeatureInheritance().isEmpty(), property -> Arrays
+			.asList(Constants.CLAFER_PRIMITIVE_TYPES).contains(property.getPropertyType()));
 
 		claferModel = new ClaferModel();
 		ClaferFeature taskFeature = new ClaferFeature(Constants.FeatureType.ABSTRACT, "Task", "");
@@ -48,6 +56,15 @@ public class ClaferModelContentProviderTest {
 
 		// test for ClaferFeature as an input
 		assertEquals(1, contentProvider.getChildren(claferModel.getClaferModel().get(0)).length);
+
+		// with filters attached
+
+		// models as an input still don't yield any children
+		assertNull(contentProviderWithFilters.getChildren(claferModel));
+
+		// does the abstract Task clafer have primitive fields? yes, the description
+		ClaferProperty property = ((ClaferProperty) contentProviderWithFilters.getChildren(claferModel.getClaferModel().get(0))[0]);
+		assertEquals("description", property.getPropertyName());
 	}
 
 	@Test
@@ -56,6 +73,14 @@ public class ClaferModelContentProviderTest {
 		assertEquals(2, contentProvider.getElements(claferModel).length);
 		// test for ClaferFeature as an input
 		assertEquals(0, contentProvider.getElements(claferModel.getClaferModel().get(0)).length);
+
+		// with filters attached
+
+		// ClaferModel as an input, abstract Task does not match the filter, as it does not inherit
+		assertEquals(1, contentProviderWithFilters.getElements(claferModel).length);
+
+		// ClaferFeature as an input, does not make sense for getElements
+		assertEquals(0, contentProviderWithFilters.getElements(claferModel.getClaferModel().get(0)).length);
 	}
 
 	@Test
@@ -64,8 +89,10 @@ public class ClaferModelContentProviderTest {
 
 		// test for ClaferModel as an input
 		assertNull(contentProvider.getParent(claferModel));
+		assertNull(contentProviderWithFilters.getParent(claferModel));
 		// test for ClaferFeature as an input
 		assertNull(contentProvider.getParent(claferModel.getClaferModel().get(0)));
+		assertNull(contentProviderWithFilters.getParent(claferModel.getClaferModel().get(0)));
 	}
 
 	@Test
@@ -75,6 +102,15 @@ public class ClaferModelContentProviderTest {
 		assertEquals(false, contentProvider.hasChildren(claferModel));
 		// test for ClaferFeature as an input
 		assertEquals(true, contentProvider.hasChildren(claferModel.getClaferModel().get(0)));
+
+		// with filters attached
+
+		assertEquals(false, contentProviderWithFilters.hasChildren(claferModel));
+		// the abstract Task clafer has a property of a primitive type
+		assertEquals(true, contentProviderWithFilters.hasChildren(claferModel.getClaferModel().get(0)));
+		// the concrete SymmetricEncryption clafer has no property of a primitive type of its own
+		// (it does inherit the description field, but this is disregarded for the content provider)
+		assertEquals(false, contentProviderWithFilters.hasChildren(claferModel.getClaferModel().get(1)));
 	}
 
 }

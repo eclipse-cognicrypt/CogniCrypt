50af52508ce9b60eab58da5a10b9f3b08c495b46
Rajiv Thorat <rajivthorat2003@gmail.com>
2018-03-15 15:30:14 +0100

    Updated readability and the Javadocs

--- plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/taskintegrator/widgets/CompositeForXsl.java
+++ plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/taskintegrator/widgets/CompositeForXsl.java
@@ -24,26 +24,30 @@ import de.cognicrypt.codegenerator.taskintegrator.controllers.XmlRegionAnalyzer;
 
 
 public class CompositeForXsl extends Composite {
-	private StyledText xslTxtBox;
+
+	private StyledText xslTxtBox; // this text box needs to be accessible on the page as well.
 	
-		public CompositeForXsl(Composite parent, int style) {
-			super(parent,SWT.BORDER);
-		//this.setBounds(0,0,887,500);
+	public CompositeForXsl(Composite parent, int style) {
+		super(parent, SWT.BORDER);
 		setLayout(new GridLayout(2, false));
 			
-			//UI Widgets for xslPage
-			setXslTxtBox(new StyledText(this,SWT.FULL_SELECTION | SWT.V_SCROLL | SWT.H_SCROLL));
+		//UI Widgets for xslPage
+		setXslTxtBox(new StyledText(this, SWT.FULL_SELECTION | SWT.V_SCROLL | SWT.H_SCROLL));
 		xslTxtBox.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 2));
-			xslTxtBox.setCursor(null);
-			
-			
-		}
-		
-		public void updateTheTextFieldWithFileData(String filePath){
+		xslTxtBox.setCursor(null);
+	}
+
+	/**
+	 * Read the text from the file provided and update the {@link StyledText} with the processed data.
+	 * 
+	 * @param filePath
+	 *        the path that is chosen from the FileDialog.
+	 */
+	public void updateTheTextFieldWithFileData(String filePath) {
 						
-			StringBuilder dataFromFile = new StringBuilder();
+		StringBuilder dataFromFile = new StringBuilder();
 			
-			Path path = Paths.get(filePath);
+		Path path = Paths.get(filePath);
 			
 		if (path.getFileName().toString().endsWith(".java") || path.getFileName().toString().endsWith(".JAVA") || path.getFileName().toString().endsWith(".txt") || path
 			.getFileName().toString().endsWith(".TXT")) {
@@ -91,6 +95,9 @@ public class CompositeForXsl extends Composite {
 		
 	}
 
+	/**
+	 * Analyzes the {@link StyledText}, calculates the {@link XmlRegion} and {@link StyleRange} and applies the syntax highlight.
+	 */
 	public void colorizeTextBox() {
 		List<XmlRegion> regions = new XmlRegionAnalyzer().analyzeXml(xslTxtBox.getText());
 		List<StyleRange> ranges = computeStyle(regions);
@@ -101,6 +108,14 @@ public class CompositeForXsl extends Composite {
 
 	}
 
+	/**
+	 * Read the data from the file and add it to the StringBuilder.
+	 * 
+	 * @param dataFromFile
+	 *        the StringBuilder.
+	 * @param filePath
+	 *        the location of the file to be read.
+	 */
 	private void appendTextFromFileToStringBuilder(StringBuilder dataFromFile, String filePath) {
 		try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
 
@@ -119,66 +134,76 @@ public class CompositeForXsl extends Composite {
 
 	}
 
-		private List<StyleRange> computeStyle(List<XmlRegion> regions) {
-			List<StyleRange> styleRanges = new ArrayList<StyleRange> ();
-		    for( XmlRegion xr : regions ) {
-		 
-		        // The style itself depends on the region type
-		        // In this example, we use colors from the system
-		        StyleRange sr = new StyleRange();
-		        switch( xr.getXmlRegionType()) {
-		            case MARKUP:
-		                sr.foreground = Display.getDefault().getSystemColor(SWT.COLOR_DARK_GREEN);
-		                break;
-		 
-		            case ATTRIBUTE:
-		                sr.foreground = Display.getDefault().getSystemColor(SWT.COLOR_DARK_RED);
-		                break;
-		                
-		            case ATTRIBUTE_VALUE: 
-		            	sr.foreground = Display.getDefault().getSystemColor(SWT.COLOR_BLUE);
-		            	break;
-		            
-		            case MARKUP_VALUE: 
-		            	sr.foreground = Display.getDefault().getSystemColor(SWT.COLOR_DARK_GRAY);
-		            	break;
-		            case COMMENT: 
-		            	sr.foreground = Display.getDefault().getSystemColor(SWT.COLOR_GRAY);
-		            	break;
-		            case INSTRUCTION: 
-		            	sr.foreground = Display.getDefault().getSystemColor(SWT.COLOR_BLACK);
-		            	break;
-		            case CDATA: 
-		            	sr.foreground = Display.getDefault().getSystemColor(SWT.COLOR_DARK_GREEN);
-		            	break;
-		            case WHITESPACE: break;
-		            default: break;
-		        }
-		 
-		        // Define the position and limit
-		        sr.start = xr.getStart();
-		        sr.length = xr.getEnd() - xr.getStart();
-		        styleRanges.add( sr );
-		    }
-		 
-		    return styleRanges;
+	/**
+	 * Set the colors to all the {@link XmlRegion}.
+	 * 
+	 * @param regions
+	 *        the List of {@link XmlRegion}
+	 * @return returns the {@link StyleRange} for the given code.
+	 */
+	private List<StyleRange> computeStyle(List<XmlRegion> regions) {
+		List<StyleRange> styleRanges = new ArrayList<StyleRange>();
+		for (XmlRegion xr : regions) {
+
+			// The style itself depends on the region type
+			// In this example, we use colors from the system
+			StyleRange sr = new StyleRange();
+			switch (xr.getXmlRegionType()) {
+				case MARKUP:
+					sr.foreground = Display.getDefault().getSystemColor(SWT.COLOR_DARK_GREEN);
+					break;
+
+				case ATTRIBUTE:
+					sr.foreground = Display.getDefault().getSystemColor(SWT.COLOR_DARK_RED);
+					break;
+
+				case ATTRIBUTE_VALUE:
+					sr.foreground = Display.getDefault().getSystemColor(SWT.COLOR_BLUE);
+					break;
+
+				case MARKUP_VALUE:
+					sr.foreground = Display.getDefault().getSystemColor(SWT.COLOR_DARK_GRAY);
+					break;
+				case COMMENT:
+					sr.foreground = Display.getDefault().getSystemColor(SWT.COLOR_GRAY);
+					break;
+				case INSTRUCTION:
+					sr.foreground = Display.getDefault().getSystemColor(SWT.COLOR_BLACK);
+					break;
+				case CDATA:
+					sr.foreground = Display.getDefault().getSystemColor(SWT.COLOR_DARK_GREEN);
+					break;
+				case WHITESPACE:
+					break;
+				default:
+					break;
+			}
+
+			// Define the position and limit
+			sr.start = xr.getStart();
+			sr.length = xr.getEnd() - xr.getStart();
+			styleRanges.add(sr);
 		}
 
-		/**
-		 * @return the xslTxtBox
-		 */
-		public StyledText getXslTxtBox() {
-			return xslTxtBox;
-		}
+		return styleRanges;
+	}
 
-		/**
-		 * @param xslTxtBox the xslTxtBox to set
-		 */
-		private void setXslTxtBox(StyledText xslTxtBox) {
-			this.xslTxtBox = xslTxtBox;
-		}
+	/**
+	 * @return the xslTxtBox
+	 */
+	public StyledText getXslTxtBox() {
+		return xslTxtBox;
+	}
+
+	/**
+	 * @param xslTxtBox
+	 *        the xslTxtBox to set
+	 */
+	private void setXslTxtBox(StyledText xslTxtBox) {
+		this.xslTxtBox = xslTxtBox;
+	}
 
 		
-	}
+}
 
 

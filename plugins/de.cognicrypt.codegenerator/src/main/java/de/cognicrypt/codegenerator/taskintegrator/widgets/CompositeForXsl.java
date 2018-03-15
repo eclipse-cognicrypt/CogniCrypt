80d0d4909f1528322b921fdf45524b36d98a4bec
Rajiv Thorat <rajivthorat2003@gmail.com>
2018-03-15 15:31:10 +0100

    Moved the controller code out of CompositeForXsl

--- plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/taskintegrator/widgets/CompositeForXsl.java
+++ plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/taskintegrator/widgets/CompositeForXsl.java
@@ -1,23 +1,17 @@
 package de.cognicrypt.codegenerator.taskintegrator.widgets;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StyleRange;
 import org.eclipse.swt.custom.StyledText;
-import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 
+import de.cognicrypt.codegenerator.taskintegrator.controllers.XSLStringGeneration;
 import de.cognicrypt.codegenerator.taskintegrator.controllers.XmlRegion;
 import de.cognicrypt.codegenerator.taskintegrator.controllers.XmlRegionAnalyzer;
 
@@ -44,55 +38,10 @@ public class CompositeForXsl extends Composite {
 	 *        the path that is chosen from the FileDialog.
 	 */
 	public void updateTheTextFieldWithFileData(String filePath) {
-						
-		StringBuilder dataFromFile = new StringBuilder();
-			
-		Path path = Paths.get(filePath);
-			
-		if (path.getFileName().toString().endsWith(".java") || path.getFileName().toString().endsWith(".JAVA") || path.getFileName().toString().endsWith(".txt") || path
-			.getFileName().toString().endsWith(".TXT")) {
 
-			// Check if the XSL tags have already been created. If yes then add the text at the cursor location.
-			if (xslTxtBox.getText().contains("<?xml version=") || xslTxtBox.getText().contains("<xsl:stylesheet xmlns:xsl=")) {
-				Point selected = xslTxtBox.getSelection();
-				String xslTxtBoxContent = xslTxtBox.getText();
-
-				dataFromFile.append(xslTxtBoxContent.substring(0, selected.x));
-				dataFromFile.append("\n");
-				dataFromFile.append("<xsl:result-document href=\"\">");
-				dataFromFile.append("\n");
-				appendTextFromFileToStringBuilder(dataFromFile, filePath);
-				dataFromFile.append("\n");
-				dataFromFile.append(xslTxtBoxContent.substring(selected.y, xslTxtBoxContent.length()));
-				xslTxtBox.setCursor(xslTxtBox.getCursor());
-
-			} else {
-				dataFromFile.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
-				dataFromFile.append("<xsl:stylesheet xmlns:xsl=\"http://www.w3.org/1999/XSL/Transform\" version=\"2.0\">\n");
-				dataFromFile.append("<xsl:output method=\"text\"/>\n");
-				dataFromFile.append("<xsl:template match=\"/\">\n");
-				dataFromFile.append("package <xsl:value-of select=\"//task/Package\"/>; \n");
-				dataFromFile.append("<xsl:apply-templates select=\"//Import\"/>\n");
-
-				appendTextFromFileToStringBuilder(dataFromFile, filePath);
-
-				dataFromFile.append("\n");
-				dataFromFile.append("</xsl:template>\n");
-				dataFromFile.append("<xsl:template match=\"Import\">\n");
-				dataFromFile.append("import <xsl:value-of select=\".\"/>;\n");
-				dataFromFile.append("</xsl:template>\n");
-				dataFromFile.append("</xsl:stylesheet>");
-			}
-
-		} else {
-			appendTextFromFileToStringBuilder(dataFromFile, filePath);
-		}
-		
-		xslTxtBox.setText(dataFromFile.toString());
+		getXslTxtBox().setText(XSLStringGeneration.generateXSLStringFromPath(filePath, getXslTxtBox().getText(), getXslTxtBox().getSelection(), null));
 
 		colorizeTextBox();
-
-		
 	}
 
 	/**
@@ -103,36 +52,11 @@ public class CompositeForXsl extends Composite {
 		List<StyleRange> ranges = computeStyle(regions);
 
 		for (StyleRange styleRange : ranges) {
-			xslTxtBox.setStyleRange(styleRange);
+			getXslTxtBox().setStyleRange(styleRange);
 		}
 
 	}
 
-	/**
-	 * Read the data from the file and add it to the StringBuilder.
-	 * 
-	 * @param dataFromFile
-	 *        the StringBuilder.
-	 * @param filePath
-	 *        the location of the file to be read.
-	 */
-	private void appendTextFromFileToStringBuilder(StringBuilder dataFromFile, String filePath) {
-		try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-
-			String line = br.readLine();
-
-			while (line != null) {
-				dataFromFile.append(line);
-				dataFromFile.append(System.lineSeparator());
-				line = br.readLine();
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-	}
 
 	/**
 	 * Set the colors to all the {@link XmlRegion}.

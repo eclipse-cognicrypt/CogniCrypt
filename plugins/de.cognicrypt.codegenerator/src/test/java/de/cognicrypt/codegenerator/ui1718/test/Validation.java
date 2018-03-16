d20d86252aabba8cb3ea5127fb4a6b483d26f194
Anan Ameez Kudukkathu Valappil <ananameezkv@gmail.com>
2018-03-16 18:13:56 +0100

    Unit testing for the text fields

---
+++ plugins/de.cognicrypt.codegenerator/src/test/java/de/cognicrypt/codegenerator/ui1718/test/Validation.java
@@ -0,0 +1,86 @@
+package de.cognicrypt.codegenerator.ui1718.test;
+
+public class Validation {
+
+	public int validationInteger(String testInput) {
+		 
+		//count=0 is the input accepting case
+		//count=number of error
+		 
+		int count = 0;
+		char[] chars = new char[testInput.length()];
+		testInput.getChars(0, chars.length, chars, 0);
+		for (int i = 0; i < testInput.length(); i++) {
+			if (!('0' <= chars[i] && chars[i] <= '9')) {
+				count++;
+			}
+		}
+
+		return count;
+	}
+
+	public int validationPortNumber(String testInput) {
+
+		//count=0 is the input accepting case 
+		//count=1 is the error case
+		
+		int count = 0;
+		String port = testInput;
+		try {
+			int portNum = Integer.valueOf(port);
+			if (portNum < 0 || portNum > 65535) {
+				count++;
+			}
+		} catch (NumberFormatException ex) {
+			if (!port.equals("")){
+				count++;
+			}
+		}
+		
+		return count;
+	}
+	
+
+	public int validationIpAddress(String testInput) {
+
+		//count=0 is the input accepting case 
+		//count=1 is the error case
+		
+		int count = 0;
+		String ip = testInput;
+		try {
+			if (ip != null && !ip.isEmpty()) {
+				String[] ipAddress = ip.split("\\.");
+				if (ipAddress.length == 4) {
+					
+					for (int i = 0; i <= ipAddress.length - 1; i++) {
+						int j = Integer.parseInt(ipAddress[i]);
+						if (j < 0 || j > 255 || j < 0) {
+							count++;
+						}
+					}
+					if (ip.endsWith(".")) {
+						count++;
+					}
+
+					if (ip.endsWith("..")) {
+						count++;
+					}
+
+					if (ip.startsWith(".")) {
+						count++;
+					}
+				}
+				else{
+					count++;
+				}
+
+			}
+		} catch (NumberFormatException ex) {
+			if (!ip.equals("")){
+				count++;
+			}
+		}
+		return count;
+	}
+}
---
+++ plugins/de.cognicrypt.codegenerator/src/test/java/de/cognicrypt/codegenerator/ui1718/test/Validation_integer.java
@@ -0,0 +1,23 @@
+package de.cognicrypt.codegenerator.ui1718.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class Validation_integer {
+
+	@Test
+	public void test() {
+		Validation integer = new Validation();
+		int Output1 = integer.validationInteger("3");
+		assertEquals(0, Output1);
+		int Output2 = integer.validationInteger("abcd");
+		assertEquals(4, Output2);
+		int Output3 = integer.validationInteger("3a");
+		assertEquals(1, Output3);
+		int Output4 = integer.validationInteger("3a ");
+		assertEquals(2, Output4);
+	}
+
+}

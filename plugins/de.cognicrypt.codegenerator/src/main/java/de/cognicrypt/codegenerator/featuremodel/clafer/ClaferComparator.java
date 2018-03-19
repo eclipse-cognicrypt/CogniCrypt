1323bb5a39e4beacac7e5c085ddb90498f3fcbf3
André Birke <abirke@campus.uni-paderborn.de>
2018-03-19 20:29:47 +0100

    consider non-int security parameter when sorting instance list
    The security parameter will often be a Security instance, i.e. a reference clafer targeting integer. This type of security parameter will now be considered for sorting in the instance list. The Comparator is outsourced to be testable.
    
    Former-commit-id: ad701c39dee44eb0d7f6ce9304b93a84c64b204a

---
+++ plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/featuremodel/clafer/ClaferComparator.java
@@ -0,0 +1,35 @@
+package de.cognicrypt.codegenerator.featuremodel.clafer;
+
+import java.util.Comparator;
+
+import org.clafer.instance.InstanceClafer;
+
+public class ClaferComparator implements Comparator<InstanceClafer> {
+
+	@Override
+	public int compare(final InstanceClafer left, final InstanceClafer right) {
+		return -Integer.compare(getSecurityLevel(left), getSecurityLevel(right));
+	}
+
+	private Integer getSecurityLevel(final InstanceClafer instance) {
+		for (final InstanceClafer innerInst : instance.getChildren()) {
+			// check if the clafer has a child named security
+			if (innerInst.getType().getName().contains("security")) {
+				final Object securityClafer = innerInst.getRef();
+				// if the security clafer is an integer, use it for sorting
+				if (securityClafer instanceof Integer) {
+					return (Integer) securityClafer;
+				}
+				// if the security clafer is a reference clafer (e.g., a child of "abstract Security : Enum -> integer") use the target value of the reference for sorting
+				else if (securityClafer instanceof InstanceClafer) {
+					InstanceClafer instanceClaferSecurity = (InstanceClafer) securityClafer;
+					if (instanceClaferSecurity.getRef() instanceof Integer) {
+						return (Integer) instanceClaferSecurity.getRef();
+					}
+				}
+			}
+		}
+		return -1;
+	}
+
+}

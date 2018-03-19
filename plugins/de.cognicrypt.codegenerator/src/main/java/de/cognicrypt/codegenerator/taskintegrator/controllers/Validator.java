ca40e6a2ba552a83c73b450d415d51afdc62ada8
Rajiv Thorat <rajivthorat2003@gmail.com>
2018-03-19 16:21:50 +0100

    Removing unnecessary parameter

--- plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/taskintegrator/controllers/Validator.java
+++ plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/taskintegrator/controllers/Validator.java
@@ -2,14 +2,12 @@ package de.cognicrypt.codegenerator.taskintegrator.controllers;
 
 import java.util.List;
 
-import org.eclipse.jface.fieldassist.ControlDecoration;
-
 import de.cognicrypt.codegenerator.tasks.Task;
 import de.cognicrypt.codegenerator.tasks.TaskJSONReader;
 
 public class Validator {
 
-	public static boolean checkIfTaskNameAlreadyExists(String taskName, ControlDecoration controlDecoration) {
+	public static boolean checkIfTaskNameAlreadyExists(String taskName) {
 		List<Task> existingTasks = TaskJSONReader.getTasks(); // Required to validate the task name that is chosen by the user.
 		boolean validString = true;
 

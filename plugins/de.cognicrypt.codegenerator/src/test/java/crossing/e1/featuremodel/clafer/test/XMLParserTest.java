7652812382d51519147aa6589de8a35536abc7e1
André Birke <abirke@campus.uni-paderborn.de>
2018-03-26 15:36:57 +0200

    parameterize XMLParserTest to test multiple models
    This commit uses org.junit.runner.Parameterized to
    test two different xml instance files. One of them
    contains an element tag instead of an algorithm
    tag which had not been considered before.
    
    
    Former-commit-id: aca4e1fda72a1d52fb20be84690c60a1d915aaca

--- plugins/de.cognicrypt.codegenerator/src/test/java/crossing/e1/featuremodel/clafer/test/XMLParserTest.java
+++ plugins/de.cognicrypt.codegenerator/src/test/java/crossing/e1/featuremodel/clafer/test/XMLParserTest.java
@@ -5,6 +5,10 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 
 import org.clafer.instance.InstanceClafer;
@@ -12,6 +16,9 @@ import org.dom4j.DocumentException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import de.cognicrypt.codegenerator.Constants;
 import de.cognicrypt.codegenerator.featuremodel.clafer.ClaferModel;
@@ -21,21 +28,31 @@ import de.cognicrypt.codegenerator.question.Question;
 import de.cognicrypt.codegenerator.utilities.FileHelper;
 import de.cognicrypt.codegenerator.utilities.XMLParser;
 
+@RunWith(value = Parameterized.class)
 public class XMLParserTest {
 
 	ClaferModel claferModel;
 	InstanceGenerator instGen;
 	HashMap<Question, Answer> constraints;
 	InstanceClafer inst;
-	String validFilePath = "src/test/resources/valid.xml";
+
+	// parameterized by junit.runners.Parameterized (see the constructor)
+	String jsFilePath;
+	String validFilePath;
+
+	// temporary output file
+	// common for all tests (among different parameterizations)
 	String xmlTestFilePath = "src/test/testXMLwriteInstance.xml";
+
+	public XMLParserTest(String jsFile, String xmlFile) {
+		this.jsFilePath = jsFile;
+		this.validFilePath = xmlFile;
+	}
 
 	@Before
 	public void setUp() throws Exception {
-		String path = "src/main/resources/ClaferModel/CryptoTasks.js";
-		path = "src/test/resources/hashing.js";
-		this.claferModel = new ClaferModel(path);
-		this.instGen = new InstanceGenerator(path, "PasswordBasedEncryption", "description");
+		this.claferModel = new ClaferModel(jsFilePath);
+		this.instGen = new InstanceGenerator(jsFilePath, "PasswordBasedEncryption", "description");
 		this.constraints = new HashMap<>();
 		this.inst = this.instGen.generateInstances(this.constraints).get(0);
 	}
@@ -43,6 +60,12 @@ public class XMLParserTest {
 	@After
 	public void tearDown() {
 		FileHelper.deleteFile(this.xmlTestFilePath);
+	}
+
+	@Parameters(name = "testXmlParser{index}({0},{1})")
+	public static Collection<Object[]> data() {
+		return Arrays.asList(
+			new Object[][] { { "src/test/resources/hashing.js", "src/test/resources/validHashing.xml" }, { "src/test/resources/security.js", "src/test/resources/validSecurity.xml" } });
 	}
 
 	@Test
@@ -73,11 +96,21 @@ public class XMLParserTest {
 			importBuilder.append(importSt);
 			importBuilder.append("</Import>");
 		}
-		
-		final String validXML = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<task description=\"Main\"><Package>Crypto</Package><Imports>"+ importBuilder.toString() + "</Imports><algorithm type=\"Digest\"><outputSize>256</outputSize><name>SHA3-256</name><performance>3</performance><status>secure</status></algorithm><code/></task>";
+
+		final String encoding = "UTF-8";
+		byte[] encoded = Files.readAllBytes(Paths.get(this.validFilePath));
+		String validXML = new String(encoded, encoding);
+
 		final XMLParser xmlparser = new XMLParser();
 
 		final String xml = xmlparser.displayInstanceValues(this.inst, this.constraints).asXML();
-		assertEquals(validXML, xml);
+		assertEquals(uglifyXML(validXML), uglifyXML(xml));
+	}
+
+	/**
+	 * move all tags together and remove newlines
+	 */
+	public String uglifyXML(String input) {
+		return input.replaceAll(">\\s*<", "><").replace("\n", "");
 	}
 }

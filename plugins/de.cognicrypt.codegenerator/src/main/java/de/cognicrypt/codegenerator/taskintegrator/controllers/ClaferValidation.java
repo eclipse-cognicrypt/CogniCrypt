fb23e664e06d9a17bfd6c721ee380d64187f24c7
André Birke <abirke@campus.uni-paderborn.de>
2018-03-28 09:01:16 +0200

    disallow special characters and number prefixes in Clafer validation
    Former-commit-id: d6ac01ab682a506f9a23a524a1e3b29d2bf3f526

--- plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/taskintegrator/controllers/ClaferValidation.java
+++ plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/taskintegrator/controllers/ClaferValidation.java
@@ -6,17 +6,43 @@ import de.cognicrypt.codegenerator.UIConstants;
 
 public class ClaferValidation {
 
-	public static boolean validateClaferName(String clafer, ControlDecoration decoration) {
+	/**
+	 * check if the given string is a valid clafer name and return a corresponding message
+	 * 
+	 * @param claferName
+	 *        {@link String} name to be tested
+	 * @return {@link String} message containing errors in the name, empty string if valid
+	 */
+	public static String getValidationMessage(String claferName) {
+		if (claferName.contains(" ")) {
+			return "The name must not contain any spaces";
+		} else if (claferName.isEmpty()) {
+			return "Please enter a name";
+		} else if (claferName.matches("^[0-9].*")) {
+			return "The name must not begin with a number";
+		} else if (!claferName.matches("^[a-zA-Z0-9_-]*$")) {
+			return "The name must not contain any special characters";
+		}
+
+		return "";
+	}
+
+	/**
+	 * check if the given string is a valid clafer name and use the {@link ControlDecoration} to give feedback
+	 * 
+	 * @param claferName
+	 *        {@link String} name to be tested
+	 * @param decoration
+	 *        {@link ControlDecoration} to display the error message in, hide if the string is valid
+	 * @return
+	 */
+	public static boolean validateClaferName(String claferName, ControlDecoration decoration) {
 		boolean valid = true;
 
-		if (clafer.contains(" ")) {
+		String result = getValidationMessage(claferName);
+		if (!result.isEmpty()) {
 			decoration.setImage(UIConstants.DEC_ERROR);
-			decoration.setDescriptionText("The name must not contain any spaces");
-			decoration.show();
-			valid = false;
-		} else if (clafer.isEmpty()) {
-			decoration.setImage(UIConstants.DEC_ERROR);
-			decoration.setDescriptionText("Please enter a name");
+			decoration.setDescriptionText(result);
 			decoration.show();
 			valid = false;
 		} else {

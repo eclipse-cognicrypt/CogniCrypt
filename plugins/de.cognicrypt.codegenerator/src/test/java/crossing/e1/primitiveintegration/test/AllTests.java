d47df7e6f9b555916d5936c3715225aa6a5cc6c1
Ahmed <ahmed_bentaher@ymail.com>
2018-04-02 00:00:15 +0200

    minor update in testing
    Former-commit-id: 82675787a9bf43f4a8b6c254090ba82f2e11741f

--- plugins/de.cognicrypt.codegenerator/src/test/java/crossing/e1/primitiveintegration/test/AllTests.java
+++ plugins/de.cognicrypt.codegenerator/src/test/java/crossing/e1/primitiveintegration/test/AllTests.java
@@ -1,15 +1,8 @@
 package crossing.e1.primitiveintegration.test;
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-
-import crossing.e1.featuremodel.clafer.test.ClaferModelUtilsTest;
-import crossing.e1.featuremodel.clafer.test.QuestionReaderTest;
-import crossing.e1.featuremodel.clafer.test.XMLParserTest;
 
 
 @RunWith(Suite.class)

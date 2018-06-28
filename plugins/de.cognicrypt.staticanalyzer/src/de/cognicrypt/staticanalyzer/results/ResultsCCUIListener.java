d3b262de7956ca9735dfb160268d2aa28c0eb48d
Stefan Krueger <stefan.krueger@crossing.tu-darmstadt.de>
2018-06-28 14:15:38 +0200

    Updated analysis-results listener to new version.

--- plugins/de.cognicrypt.staticanalyzer/src/de/cognicrypt/staticanalyzer/results/ResultsCCUIListener.java
+++ plugins/de.cognicrypt.staticanalyzer/src/de/cognicrypt/staticanalyzer/results/ResultsCCUIListener.java
@@ -1,29 +1,22 @@
 package de.cognicrypt.staticanalyzer.results;
 
 import java.util.Collection;
-import java.util.Map.Entry;
-import java.util.Set;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 
 import com.google.common.collect.Multimap;
-import com.google.common.collect.Table;
 
-import boomerang.BackwardQuery;
-import boomerang.Query;
 import boomerang.jimple.Statement;
 import boomerang.jimple.Val;
 import boomerang.results.ForwardBoomerangResults;
 import crypto.analysis.AnalysisSeedWithSpecification;
-import crypto.analysis.CrySLAnalysisListener;
-import crypto.analysis.EnsuredCryptSLPredicate;
 import crypto.analysis.IAnalysisSeed;
+import crypto.analysis.ICrySLResultsListener;
 import crypto.analysis.errors.AbstractError;
 import crypto.extractparameter.CallSiteWithParamIndex;
 import crypto.extractparameter.ExtractedValue;
 import crypto.interfaces.ISLConstraint;
-import crypto.rules.CryptSLPredicate;
 import de.cognicrypt.staticanalyzer.Activator;
 import de.cognicrypt.utils.Utils;
 import soot.SootClass;
@@ -36,7 +29,7 @@ import typestate.TransitionFunction;
  * @author Stefan Krueger
  *
  */
-public class ResultsCCUIListener extends CrySLAnalysisListener {
+public class ResultsCCUIListener implements ICrySLResultsListener {
 
 	private final ErrorMarkerGenerator markerGenerator;
 	private final IProject currentProject;
@@ -59,11 +52,6 @@ public class ResultsCCUIListener extends CrySLAnalysisListener {
 		this.markerGenerator.addMarker(unitToResource(error.getErrorLocation()), error.getErrorLocation().getUnit().get().getJavaSourceStartLineNumber(), message);
 	}
 
-	@Override
-	public void predicateContradiction(final Node<Statement, Val> location, final Entry<CryptSLPredicate, CryptSLPredicate> arg1) {
-		this.markerGenerator.addMarker(unitToResource(location.stmt()), location.stmt().getUnit().get().getJavaSourceStartColumnNumber(), "Predicate mismatch");
-	}
-
 	private IResource unitToResource(final Statement stmt) {
 		final SootClass className = stmt.getMethod().getDeclaringClass();
 		try {
@@ -75,46 +63,6 @@ public class ResultsCCUIListener extends CrySLAnalysisListener {
 		return this.currentProject.getFile("src/" + className.getName().replace(".", "/") + ".java");
 	}
 
-	@Override
-	public void afterAnalysis() {
-		// Nothing
-	}
-
-	@Override
-	public void afterConstraintCheck(final AnalysisSeedWithSpecification arg0) {
-		// nothing
-	}
-
-	@Override
-	public void afterPredicateCheck(final AnalysisSeedWithSpecification arg0) {
-		// Nothing
-	}
-
-	@Override
-	public void beforeAnalysis() {
-		// Nothing
-
-	}
-
-	@Override
-	public void beforeConstraintCheck(final AnalysisSeedWithSpecification arg0) {
-		// Nothing
-	}
-
-	@Override
-	public void beforePredicateCheck(final AnalysisSeedWithSpecification arg0) {
-		// Nothing
-	}
-
-	@Override
-	public void boomerangQueryFinished(final Query arg0, final BackwardQuery arg1) {
-		// Nothing
-	}
-
-	@Override
-	public void boomerangQueryStarted(final Query arg0, final BackwardQuery arg1) {
-		// Nothing
-	}
 
 	@Override
 	public void checkedConstraints(final AnalysisSeedWithSpecification arg0, final Collection<ISLConstraint> arg1) {
@@ -128,20 +76,10 @@ public class ResultsCCUIListener extends CrySLAnalysisListener {
 	}
 
 	@Override
-	public void ensuredPredicates(final Table<Statement, Val, Set<EnsuredCryptSLPredicate>> arg0, final Table<Statement, IAnalysisSeed, Set<CryptSLPredicate>> arg1, final Table<Statement, IAnalysisSeed, Set<CryptSLPredicate>> arg2) {
-		// Nothing
-	}
-
-
-	@Override
 	public void onSeedTimeout(final Node<Statement, Val> arg0) {
 		//Nothing
 	}
 
-	@Override
-	public void seedStarted(final IAnalysisSeed arg0) {
-		// Nothing
-	}
 
 	@Override
 	public void collectedValues(AnalysisSeedWithSpecification arg0,

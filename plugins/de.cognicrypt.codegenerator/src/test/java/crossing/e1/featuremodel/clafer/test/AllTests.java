b9dbdc06d9fbef1046e91e718808397d218137cd
Stefan Krueger <stefan.krueger@crossing.tu-darmstadt.de>
2018-07-23 06:01:53 +0200

    Added license headers to fix #203.
    Former-commit-id: 182cba985759dbd444738428b572f8ee3d272763

--- plugins/de.cognicrypt.codegenerator/src/test/java/crossing/e1/featuremodel/clafer/test/AllTests.java
+++ plugins/de.cognicrypt.codegenerator/src/test/java/crossing/e1/featuremodel/clafer/test/AllTests.java
@@ -1,3 +1,13 @@
+/********************************************************************************
+ * Copyright (c) 2015-2018 TU Darmstadt
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ ********************************************************************************/
+
 package crossing.e1.featuremodel.clafer.test;
 
 import org.junit.runner.RunWith;

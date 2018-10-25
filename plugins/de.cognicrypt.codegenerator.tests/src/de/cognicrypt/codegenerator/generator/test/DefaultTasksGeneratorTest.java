12093ebf5e4d1fc2a5e18440f2ff84d0763fcb6a
AndreSonntag <Andre-Sonntag@gmx.de>
2018-10-25 14:06:07 +0200

    Refactoring

--- plugins/de.cognicrypt.codegenerator.tests/src/de/cognicrypt/codegenerator/generator/test/DefaultTasksGeneratorTest.java
+++ plugins/de.cognicrypt.codegenerator.tests/src/de/cognicrypt/codegenerator/generator/test/DefaultTasksGeneratorTest.java
@@ -10,7 +10,7 @@ import org.eclipse.jdt.core.JavaModelException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import de.cognicrypt.codegenerator.DeveloperProject;
+import de.cognicrypt.client.DeveloperProject;
 import de.cognicrypt.codegenerator.generator.CodeGenerator;
 import de.cognicrypt.codegenerator.generator.XSLBasedGenerator;
 import de.cognicrypt.codegenerator.tasks.Task;

946731aae77eb6405859b27b6cbac53d6dbb9969
Shahrzad <shahrzad.asghari73@gmail.com>
2019-02-22 16:44:23 +0100

    commented unimplemented properties
    Signed-off-by: Shahrzad <shahrzad.asghari73@gmail.com>

--- plugins/de.cognicrypt.codegenerator/src/main/java/properties/CogniCryptPreferenceInitializer.java
+++ plugins/de.cognicrypt.codegenerator/src/main/java/properties/CogniCryptPreferenceInitializer.java
@@ -15,13 +15,13 @@ public class CogniCryptPreferenceInitializer extends AbstractPreferenceInitializ
         
 		IPreferenceStore store = Activator.getDefault().getPreferenceStore();
 		store.setDefault(ICogniCryptConstants.PRE_COMBO, 0);
-		store.setDefault(ICogniCryptConstants.PRE_CHECKBOX1, false);
-		store.setDefault(ICogniCryptConstants.PRE_CHECKBOX2, false);
+//		store.setDefault(ICogniCryptConstants.PRE_CHECKBOX1, false);
+//		store.setDefault(ICogniCryptConstants.PRE_CHECKBOX2, false);
 		store.setDefault(ICogniCryptConstants.PRE_CHECKBOX3, false);
 		store.setDefault(ICogniCryptConstants.PRE_CHECKBOX4, false);
-		store.setDefault(ICogniCryptConstants.PRE_ADV_COMBO1,0);
+//		store.setDefault(ICogniCryptConstants.PRE_ADV_COMBO1,0);
 //		store.setDefault(ICogniCryptConstants.PRE_ADV_COMBO2,0);
-		store.setDefault(ICogniCryptConstants.PRE_ADV_COMBO3,0);
+//		store.setDefault(ICogniCryptConstants.PRE_ADV_COMBO3,0);
 
 	}
 

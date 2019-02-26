6b853691a5ac471838be4ac79e7586dc34492839
Stefan Krueger <stefan.krueger@crossing.tu-darmstadt.de>
2019-02-26 01:43:57 +0100

    Formatting.
    Former-commit-id: 413340d6af71ad8acc0ccb586f3c13905f91ff3e

--- plugins/de.cognicrypt.staticanalyzer/src/de/cognicrypt/staticanalyzer/view/AnalysisData.java
+++ plugins/de.cognicrypt.staticanalyzer/src/de/cognicrypt/staticanalyzer/view/AnalysisData.java
@@ -2,43 +2,42 @@ package de.cognicrypt.staticanalyzer.view;
 
 import java.util.ArrayList;
 
-
 /**
  * This class contains the seeds , errors and health information from all the classes that were analysed and reached ResultsCCUIListener.
+ * 
  * @author Adnan Manzoor
- *
  */
 public class AnalysisData {
 	ArrayList<String> seeds;
 	ArrayList<String> errors;
 	String health;
-	
-	public AnalysisData(){
+
+	public AnalysisData() {
 		this.seeds = new ArrayList<>();
 		this.errors = new ArrayList<>();
 		this.health = "Healthy";
 	}
-	
+
 	public void addSeed(String seed) {
 		seeds.add(seed);
 	}
-	
-	public ArrayList<String> getSeeds(){
+
+	public ArrayList<String> getSeeds() {
 		return seeds;
 	}
-	
+
 	public void addError(String error) {
 		errors.add(error);
 	}
-	
+
 	public ArrayList<String> getErrors() {
 		return errors;
 	}
-	
+
 	public void setHealth(String health) {
-		this.health=health;
+		this.health = health;
 	}
-	
+
 	public String getHealth() {
 		return health;
 	}

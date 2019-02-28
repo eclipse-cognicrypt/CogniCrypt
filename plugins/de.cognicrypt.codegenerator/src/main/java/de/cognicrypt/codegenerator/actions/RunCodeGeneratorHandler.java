c9d6897c821c84873fe45d0fd79b39fcbbf2d4cc
Stefan Krueger <stefan.krueger@crossing.tu-darmstadt.de>
2019-02-28 23:54:42 +0100

    Updated extension points for popup menus and grouped them into one.

---
+++ plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/actions/RunCodeGeneratorHandler.java
@@ -0,0 +1,23 @@
+package de.cognicrypt.codegenerator.actions;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.PlatformUI;
+
+import de.cognicrypt.codegenerator.wizard.AltConfigWizard;
+import de.cognicrypt.codegenerator.wizard.CogniCryptWizardDialog;
+import de.cognicrypt.core.Constants;
+
+public class RunCodeGeneratorHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		Constants.WizardActionFromContextMenuFlag = true;
+		final CogniCryptWizardDialog dialog = new CogniCryptWizardDialog(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), new AltConfigWizard());
+		dialog.setHelpAvailable(false);
+		
+		return dialog.open();
+	}
+
+}
---
+++ plugins/de.cognicrypt.staticanalyzer/src/de/cognicrypt/staticanalyzer/handlers/RunAnalysisHandler.java
@@ -0,0 +1,25 @@
+package de.cognicrypt.staticanalyzer.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.JavaCore;
+import de.cognicrypt.utils.Utils;
+
+public class RunAnalysisHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		final AnalysisKickOff akf = new AnalysisKickOff();
+		IProject ip = Utils.getCurrentlySelectedIProject();
+		final IJavaElement iJavaElement = JavaCore.create(ip);
+		akf.setUp(iJavaElement);
+		akf.run();
+		return null;
+	}
+
+	
+
+}

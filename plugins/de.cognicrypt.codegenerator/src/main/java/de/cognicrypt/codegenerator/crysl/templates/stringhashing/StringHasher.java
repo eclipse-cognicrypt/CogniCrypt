a85e33c64f1bd8250c7da0f1a155e2398f9ff93d
Stefan Krueger <stefan.krueger@crossing.tu-darmstadt.de>
2019-08-24 14:24:33 +0200

    Completing String Hashing template.

--- plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/crysl/templates/stringhashing/StringHasher.java
+++ plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/crysl/templates/stringhashing/StringHasher.java
@@ -11,13 +11,17 @@ public class StringHasher {
 
 	public static java.lang.String createHash(String msg) throws GeneralSecurityException {
 		byte[] plainBytes = msg.getBytes(StandardCharsets.UTF_8);
-		byte[] out = null;
-		
-		CrySLCodeGenerator.getInstance().considerCrySLRule("java.security.MessageDigest").addParameter(plainBytes, "digest", 0).addReturnObject(out).generate();
-
-		String hashString = Base64.getEncoder().encodeToString(out);
-
-		return hashString;
+		byte[] out = null; 
+		CrySLCodeGenerator.getInstance().considerCrySLRule("java.security.MessageDigest").addReturnObject(out).generate();
+		return Base64.getEncoder().encodeToString(out);
 	}
+	
+	public static boolean verifyHash(java.lang.String newMsg, java.lang.String compareeHash) {
+		byte[] plainBytes = newMsg.getBytes(StandardCharsets.UTF_8);
+		byte[] out = null; 
+		CrySLCodeGenerator.getInstance().considerCrySLRule("java.security.MessageDigest").addReturnObject(out).generate();
+		return Base64.getEncoder().encodeToString(out).equals(newMsg);
+	}
+	
 
 }

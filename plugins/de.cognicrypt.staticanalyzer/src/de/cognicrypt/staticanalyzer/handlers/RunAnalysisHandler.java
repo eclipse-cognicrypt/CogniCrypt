e31c6150b08d5089228040a0dca44d26e89b37ba
Shahrzad <shahrzad.asghari73@gmail.com>
2019-09-10 12:33:13 +0200

    removed irrelevant code for this branch
    Signed-off-by: Shahrzad <shahrzad.asghari73@gmail.com>

--- plugins/de.cognicrypt.staticanalyzer/src/de/cognicrypt/staticanalyzer/handlers/RunAnalysisHandler.java
+++ plugins/de.cognicrypt.staticanalyzer/src/de/cognicrypt/staticanalyzer/handlers/RunAnalysisHandler.java
@@ -1,75 +1,25 @@
 package de.cognicrypt.staticanalyzer.handlers;
 
-
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.UIManager;
-
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.JavaCore;
-
-import de.cognicrypt.core.Constants;
 import de.cognicrypt.utils.Utils;
 
 public class RunAnalysisHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
+		
 		final AnalysisKickOff akf = new AnalysisKickOff();
 		IProject ip = Utils.getCurrentlySelectedIProject();
-		
-//		check if the open editor is in the same project
-		if (ip == Utils.getCurrentProject()) {
-			try {
-//				check if there are unsaved changes
-				if (Utils.getCurrentlyOpenEditor().isDirty()) {
-					int answr = saveFile(Utils.getCurrentlyOpenFile());
-//					save file and analyse
-					if (answr == JOptionPane.NO_OPTION) {
-					}
-//					do not save file and analyse
-					else if(answr == JOptionPane.YES_OPTION) {
-						Utils.getCurrentlyOpenEditor().doSave(null);	
-					}
-//					no analyse no save file
-					else if (answr == JOptionPane.CLOSED_OPTION) {
-						return null;
-					}
-				}
-			}
-			catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
 		final IJavaElement iJavaElement = JavaCore.create(ip);
 		if (akf.setUp(iJavaElement)) {
 			akf.run();
 		}
 		return null;
 	}
-
-	public static int saveFile(IFile openFileInEditor) throws Exception {
-		
-	    JFrame frame = new JFrame();
-	    String message = "<html> The file below has been changed, would you like to save it before analysis?  <br><br>" + openFileInEditor;
-	    String folderPath = Utils.getResourceFromWithin(Constants.COGNICRYPT_ICON_DIR, de.cognicrypt.core.Activator.PLUGIN_ID).getAbsolutePath();
-	    ImageIcon icon = new ImageIcon(folderPath);
-	   
-
-	    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-	    JLabel label = new JLabel(message);
-	    
-	    return JOptionPane.showConfirmDialog(frame, label, "CogniCrypt Analysis", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, icon);
-	    
-	  }
-	
 
 }

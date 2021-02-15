61eb7e2097d6a6f7101b1c949483f6ee3f248cbd
Shahrzad <shahrzad.asghari73@gmail.com>
2021-02-15 12:57:24 +0100

    convert returns to return to reproduce the return error
    Signed-off-by: Shahrzad <shahrzad.asghari73@gmail.com>

--- plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/crysl/templates/securepassword/PasswordHasher.java
+++ plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/crysl/templates/securepassword/PasswordHasher.java
@@ -30,7 +30,7 @@ public class PasswordHasher {
 	 * 
 	 *
 	 * @param pwd the password to be hashed.
-	 * @returns the salt and hashed password in a string with a colon in between.
+	 * @return the salt and hashed password in a string with a colon in between.
 	 * @throws GeneralSecurityException This exception is thrown if a security-related exception happens that extends this general exception.
 	 * @throws NoSuchAlgorithmException This exception is thrown if no Provider supports a SecretKeyFactorySpi or SecureRadnomSpi implementation for the specified algorithms.
 	 * @throws InvalidKeySpecException This exception is thrown when key specifications are invalid.
@@ -51,7 +51,7 @@ public class PasswordHasher {
 	 * Breaks the input hashed password into two parts, salt and hash. Verifies if the input password generates the same hash using the same hash algorithm and the salt.
 	 * @param pwd the password.
 	 * @param pwdhash the hashed password with its salt.
-	 * @returns true, if the hashed input password is the equal to input hashed password.
+	 * @return true, if the hashed input password is the equal to input hashed password.
 	 * @throws GeneralSecurityException This exception is thrown if a security-related exception happens that extends this general exception.
 	 * @throws NoSuchAlgorithmException This exception is thrown if no Provider supports a SecretKeyFactorySpi or SecureRadnomSpi implementation for the specified algorithms.
 	 * @throws InvalidKeySpecException This exception is thrown when key specifications are invalid.
@@ -74,7 +74,7 @@ public class PasswordHasher {
 	 *
 	 * @param a the first value.
 	 * @param b the second value.
-	 * @returns true, if the two bytes are equal.
+	 * @return true, if the two bytes are equal.
 	 */
 	private static boolean slowEquals(byte[] a, byte[] b) {
 		int diff = a.length ^ b.length;
@@ -88,7 +88,7 @@ public class PasswordHasher {
 	 * Converts an array of bytes into a string.
 	 *
 	 * @param array of bytes.
-	 * @returns a string.
+	 * @return a string.
 	 */
 	private static java.lang.String toBase64(byte[] array) {
 		return DatatypeConverter.printBase64Binary(array);
@@ -98,7 +98,7 @@ public class PasswordHasher {
 	 * Converts a string into an array of bytes.
 	 *
 	 * @param hash, a string containing lexical representation of xsd:base64Binary.
-	 * @returns the byte array created by Base64.
+	 * @return the byte array created by Base64.
 	 */
 	private static byte[] fromBase64(java.lang.String hash) {
 		return DatatypeConverter.parseBase64Binary(hash);

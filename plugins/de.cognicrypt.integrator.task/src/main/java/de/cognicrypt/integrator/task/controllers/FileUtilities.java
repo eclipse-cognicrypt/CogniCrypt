19fc47d72591f2f1c3c377e788d3e7a12fbcfdc7
felixdaas <f1298@t-online.de>
2021-02-17 12:01:16 +0100

    Updated local Data Copy

--- plugins/de.cognicrypt.integrator.task/src/main/java/de/cognicrypt/integrator/task/controllers/FileUtilities.java
+++ plugins/de.cognicrypt.integrator.task/src/main/java/de/cognicrypt/integrator/task/controllers/FileUtilities.java
@@ -78,49 +78,12 @@ public class FileUtilities {
 		setTaskName(taskName);
 		setErrors(new StringBuilder());
 	}
-
 	
-
-	
-
-	private void writeHelpFile(final String helpFileContents) {
-		final File xmlFile = new File(Utils.getResourceFromWithin(Constants.HELP_FILE_DIRECTORY_PATH),
-				getTrimmedTaskName() + Constants.XML_EXTENSION);
-
-		try {
-			final PrintWriter writer = new PrintWriter(xmlFile);
-			writer.println(helpFileContents);
-			writer.flush();
-			writer.close();
-		} catch (final FileNotFoundException e) {
-			Activator.getDefault().logError(e);
-			getErrors().append("There was a problem wrting the Help data.\n");
-		}
-
-		if (!validateXMLFile(xmlFile)) {
-			xmlFile.delete();
-			getErrors().append("The XML data is invalid.\n");
-		}
-
-	}
-
-	
-
-	public String writeCryslTemplate(final File cryslTemplateFile, final File jsonFileLocation, final File iconFile) {
-		copyFileFromPath(jsonFileLocation);
-		copyFileFromPath(iconFile);
-		copyFileFromPath(cryslTemplateFile);
-		return getErrors().toString();
-	}
-
-	
-	public String writeCryslTemplate(final HashMap<String, File> cryslTemplateFile, final File jsonFileLocation,
-			final File iconFile) {
-		copyFileFromPath(jsonFileLocation);
-		copyFileFromPath(iconFile);
+	public String writeData(final HashMap<String, File> cryslTemplateFile, final File iconFile) {
+		copyImage(iconFile);
 		for (String key : cryslTemplateFile.keySet()) {
 			try {
-				copyFileFromPath(cryslTemplateFile.get(key), key);
+				copyTemplate(cryslTemplateFile.get(key), key);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -129,17 +92,12 @@ public class FileUtilities {
 		return getErrors().toString();
 	}
 
-	public String writeCryslTemplate(final File cryslTemplateFile, final File iconFile) {
-		copyFileFromPath(iconFile);
-		copyFileFromPath(cryslTemplateFile);
-		return getErrors().toString();
-	}
-
-	public String writeCryslTemplate(final HashMap<String, File> cryslTemplateFile, final File iconFile) {
-		copyFileFromPath(iconFile);
+	public String writeData(final HashMap<String, File> cryslTemplateFile, final File iconFile, File jsonFile) {
+		copyImage(iconFile);
+		copyJSON(jsonFile);
 		for (String key : cryslTemplateFile.keySet()) {
 			try {
-				copyFileFromPath(cryslTemplateFile.get(key), key);
+				copyTemplate(cryslTemplateFile.get(key), key);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -147,25 +105,7 @@ public class FileUtilities {
 		}
 		return getErrors().toString();
 	}
-
-	/**
-	 * Validate an XML file.
-	 *
-	 * @param helpLocation
-	 * @return
-	 */
-	private boolean validateXMLFile(final File helpLocation) {
-		final SAXReader reader = new SAXReader();
-		reader.setValidation(false);
-		try {
-			reader.read(helpLocation);
-		} catch (final DocumentException e) {
-			Activator.getDefault().logError(e);
-			appendFileErrors(helpLocation.getName());
-			return false;
-		}
-		return true;
-	}
+	
 
 	/**
 	 * For the sake of reusability.
@@ -177,101 +117,9 @@ public class FileUtilities {
 		getErrors().append(fileName);
 		getErrors().append(" are invalid.");
 		getErrors().append("\n");
-	}
-
-	/**
-	 * Validate the provided JAR file before copying it to the target location.
-	 *
-	 * @param customLibLocation
-	 * @return a boolean value for the validity of the file.
-	 */
-	private boolean validateJARFile(final File customLibLocation) {
-		final boolean validFile = true;
-		// Loop through the files, since the custom library is a directory.
-		if (customLibLocation.isDirectory()) {
-			for (final File tmpLibLocation : customLibLocation.listFiles()) {
-				if (tmpLibLocation.getPath().endsWith(Constants.JAR_EXTENSION)) {
-					ZipFile customLib;
-					try {
-						customLib = new ZipFile(tmpLibLocation);
-						customLib.entries();
-						customLib.close();
-					} catch (final IOException ex) {
-						Activator.getDefault().logError(ex);
-						appendFileErrors(tmpLibLocation.getName());
-						return false;
-					}
-				}
-			}
-		}
-		return validFile;
-	}
-
-	/**
-	 * Validate the provided XSL file before copying it to the target location.
-	 *
-	 * @param xslFileLocation
-	 * @return a boolean value for the validity of the file.
-	 */
-	private boolean validateXSLFile(final File xslFileLocation) {
-		try {
-			TransformerFactory.newInstance().newTransformer(new StreamSource(xslFileLocation));
-		} catch (final TransformerConfigurationException e) {
-			Activator.getDefault().logError(e);
-			appendFileErrors(xslFileLocation.getName());
-			return false;
-		}
-		return true;
-	}
-
-	/**
-	 * Validate the provided JSON file before copying it to the target location.
-	 *
-	 * @param jsonFileLocation
-	 * @return a boolean value for the validity of the file.
-	 */
-	private boolean validateJSONFile(final File jsonFileLocation) {
-		try {
-			final Gson gson = new Gson();
-			final BufferedReader reader = new BufferedReader(new FileReader(jsonFileLocation));
-			gson.fromJson(reader, Object.class);
-			reader.close();
-			return true;
-		} catch (IOException e) {
-			Activator.getDefault().logError(e);
-			appendFileErrors(jsonFileLocation.getName());
-			return false;
-		}
-	}
-
-	/* Only works in debug mode
-	public void copyFileFromPath(final File existingFileLocation, String option) throws IOException {
-		File parentFolder = Utils.getResourceFromWithin(Constants.codeTemplateFolder, "de.cognicrypt.codegenerator");
-		File templateFolder = new File(parentFolder, getTrimmedTaskName() + option);
-			
-		if (!templateFolder.isDirectory()) {
-			templateFolder.mkdir();
-		}
-		
-		File resourceFromWithin = Utils.getResourceFromWithin(Constants.codeTemplateFolder + getTrimmedTaskName()
-				+ option + Constants.innerFileSeparator, "de.cognicrypt.codegenerator");
-		
-		File targetDirectory = new File(resourceFromWithin, getTrimmedTaskName() + Constants.JAVA_EXTENSION);
-		
-		Path path = existingFileLocation.toPath();
-		Path path2 = targetDirectory.toPath();
-		Files.copy(path, path2, StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES);		
-	}*/
+	}	
 	
-	
-	
-	public void copyFileFromPath(final File existingFileLocation, String option) throws IOException {    
-	    
-	    // Create File Structure
-	    // One Template: Generator expects templates/<Task>/<Task>.java
-	    // Multiple Templates: Generator expects templates/<Task><Id>/<Task>.java (current)
-	    
-	    //File parentFolder = Utils.getResourceFromWithin(Constants.codeTemplateFolder, "de.cognicrypt.codegenerator");
+	public void copyTemplate(final File existingFileLocation, String option) throws IOException {
 		File parentFolder = new File(Constants.ECLIPSE_LOC_TEMP_DIR);
 		File templateFolder = new File(parentFolder, getTrimmedTaskName() + option);
 		
@@ -292,40 +140,18 @@ public class FileUtilities {
 	
 
 	/**
-	 * Copy the given file to the appropriate location.
+	 * Copy the image file to the appropriate location.
 	 *
 	 * @param existingFileLocation
 	 */
-	public void copyFileFromPath(final File existingFileLocation) {
-		if (existingFileLocation.exists() && !existingFileLocation.isDirectory()) {
+	public void copyImage(final File existingFileLocation) {
 			File targetDirectory = null;
 			try {
-
-				if (existingFileLocation.getPath().endsWith(Constants.CFR_EXTENSION)) {
-					targetDirectory = new File(Constants.ECLIPSE_LOC_CLA_DIR, getTrimmedTaskName() + Constants.CFR_EXTENSION);
-				} else if (existingFileLocation.getPath().endsWith(Constants.JS_EXTENSION)) {
-					targetDirectory = new File(Constants.ECLIPSE_LOC_CLA_DIR, getTrimmedTaskName() + Constants.JS_EXTENSION);
-				} else if (existingFileLocation.getPath().endsWith(Constants.JSON_EXTENSION)) {
-					targetDirectory = new File(Constants.ECLIPSE_LOC_TASKDESC_DIR, getTrimmedTaskName() + Constants.JSON_EXTENSION);
-				} else if (existingFileLocation.getPath().endsWith(Constants.PNG_EXTENSION)) {
+				if (existingFileLocation.getPath().endsWith(Constants.PNG_EXTENSION)) {
 					targetDirectory = new File(Constants.ECLIPSE_LOC_IMG_DIR, getTrimmedTaskName() + Constants.PNG_EXTENSION);
-				} else if (existingFileLocation.getPath().endsWith(Constants.XSL_EXTENSION)) {
-					targetDirectory = new File(Constants.ECLIPSE_LOC_XSL_DIR, getTrimmedTaskName() + Constants.XSL_EXTENSION);
-				} else if (existingFileLocation.getPath().endsWith(Constants.XML_EXTENSION)) {
-					targetDirectory = new File(Utils.getResourceFromWithin(Constants.HELP_FILE_DIRECTORY_PATH,
-							"de.cognicrypt.codegenerator"), getTrimmedTaskName() + Constants.XML_EXTENSION);
-				} else if (existingFileLocation.getPath().endsWith(Constants.JAVA_EXTENSION)) {
-					File parentFolder = new File(Constants.ECLIPSE_LOC_TEMP_DIR);
-					File templateFolder = new File(parentFolder, getTrimmedTaskName());
-					if (!templateFolder.isDirectory()) {
-						templateFolder.mkdir();
-					}
-					targetDirectory = new File(Constants.ECLIPSE_LOC_TEMP_DIR + Constants.innerFileSeparator + getTrimmedTaskName() + Constants.innerFileSeparator
-							+ getTrimmedTaskName() + Constants.JAVA_EXTENSION);
 				} else {
 					throw new Exception("Unknown file type.");
 				}
-
 				Activator.getDefault().logError("CopyNonCustom " + existingFileLocation.getAbsolutePath() + " to " + targetDirectory.getAbsolutePath());
 				Files.copy(existingFileLocation.toPath(), targetDirectory.toPath(), StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES);
 
@@ -335,28 +161,32 @@ public class FileUtilities {
 				getErrors().append(existingFileLocation.getName());
 				getErrors().append("\n");
 			}
-			// If we are dealing with a custom library location.
-		} else if (existingFileLocation.exists() && existingFileLocation.isDirectory()) {
-			final File tempDirectory = new File(Utils.getResourceFromWithin(Constants.JAR_FILE_DIRECTORY_PATH),
-					getTrimmedTaskName() + Constants.innerFileSeparator);
-			tempDirectory.mkdir();
-			// Loop through all the containing files.
-			for (final File customLibFile : existingFileLocation.listFiles()) {
-				final File tmpFile = new File(
-						tempDirectory.toString() + Constants.innerFileSeparator + customLibFile.getName());
-				try {
-					Activator.getDefault().logError("CopyCustom " + customLibFile.getAbsolutePath() + " to " + tmpFile.getAbsolutePath());
-					Files.copy(customLibFile.toPath(), tmpFile.toPath(), StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES);
-				} catch (final IOException e) {
-					Activator.getDefault().logError(e);
-					getErrors().append("There was a problem copying file ");
-					getErrors().append(existingFileLocation.getName());
-					getErrors().append("\n");
-				}
-			}
-		}
 	}
 
+	/**
+	 * Copy the Question JSON file to the appropriate location.
+	 *
+	 * @param existingFileLocation
+	 */
+	public void copyJSON(final File existingFileLocation) {
+			File targetDirectory = null;
+			try {
+				if (existingFileLocation.getPath().endsWith(Constants.JSON_EXTENSION)) {
+					targetDirectory = new File(Constants.ECLIPSE_LOC_TASKDESC_DIR, getTrimmedTaskName() + Constants.JSON_EXTENSION);
+				} else {
+					throw new Exception("Unknown file type.");
+				}
+				Activator.getDefault().logError("CopyNonCustom " + existingFileLocation.getAbsolutePath() + " to " + targetDirectory.getAbsolutePath());
+				Files.copy(existingFileLocation.toPath(), targetDirectory.toPath(), StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES);
+
+			} catch (final Exception e) {
+				Activator.getDefault().logError(e);
+				getErrors().append("There was a problem copying file ");
+				getErrors().append(existingFileLocation.getName());
+				getErrors().append("\n");
+			}
+	}
+	
 	/**
 	 * Update the task.json file with the new Task.
 	 *
@@ -401,25 +231,6 @@ public class FileUtilities {
 
 		final SegregatesQuestionsIntoPages pageContent = new SegregatesQuestionsIntoPages();
 		final ArrayList<Page> pages = pageContent.getPages();
-		boolean taskHasPageHelpContent = false;
-		for (final Page page : pages) {
-			for (final Question question : page.getContent()) {
-				if (!question.getHelpText().isEmpty()) {
-					taskHasPageHelpContent = true;
-					break;
-				}
-			}
-		}
-
-		/**
-		 * creates the xml file containing the help content of the task, adds the
-		 * location of the xml file in the plugin.xml file and sets the page help id
-		 */
-		/*
-		 * try { new CreateAndModifyXmlfile(pages, getTaskName(),
-		 * taskHasPageHelpContent); } catch (IOException | ParserConfigurationException
-		 * | SAXException | TransformerException e) { e.printStackTrace(); }
-		 */
 
 		final File jsonFile = new File(
 				Constants.ECLIPSE_LOC_TASKDESC_DIR,
@@ -445,80 +256,26 @@ public class FileUtilities {
 	}
 
 	/**
-	 * @param xslFileContents
+	 * Validate the provided JSON file before copying it to the target location.
+	 *
+	 * @param jsonFileLocation
+	 * @return a boolean value for the validity of the file.
 	 */
-	private void writeXSLFile(final String xslFileContents) {
-		final File xslFile = new File(Constants.ECLIPSE_LOC_XSL_DIR,
-				getTrimmedTaskName() + Constants.XSL_EXTENSION);
-
+	private boolean validateJSONFile(final File jsonFileLocation) {
 		try {
-			final PrintWriter writer = new PrintWriter(xslFile);
-			writer.println(xslFileContents);
-			writer.flush();
-			writer.close();
-		} catch (final FileNotFoundException e) {
+			final Gson gson = new Gson();
+			final BufferedReader reader = new BufferedReader(new FileReader(jsonFileLocation));
+			gson.fromJson(reader, Object.class);
+			reader.close();
+			return true;
+		} catch (IOException e) {
 			Activator.getDefault().logError(e);
-			getErrors().append("There was a problem wrting the XSL data.\n");
-		}
-
-		if (!validateXSLFile(xslFile)) {
-			xslFile.delete();
-			getErrors().append("The XSL data is invalid.\n");
+			appendFileErrors(jsonFileLocation.getName());
+			return false;
 		}
 	}
-
-	private void writeCryslTemplateFile(final String cryslTemplateFile) {
-		final File cryslDestFile = new File(Constants.ECLIPSE_LOC_TEMP_DIR,
-				getTrimmedTaskName() + ".java");
-		try {
-			final PrintWriter writer = new PrintWriter(cryslDestFile);
-			writer.println(cryslTemplateFile);
-			writer.flush();
-			writer.close();
-		} catch (final FileNotFoundException e) {
-			Activator.getDefault().logError(e);
-			getErrors().append("There was a problem wrting the Crysl Template data.\n");
-		}
-	}
-
-	public void updateThePluginXMLFileWithHelpData(final String machineReadableTaskName) {
-		File pluginXMLFile = Utils.getResourceFromWithin(Constants.PLUGIN_XML_FILE);
-		if (!pluginXMLFile.exists()) {
-			pluginXMLFile = Utils.getResourceFromWithin("src" + Constants.innerFileSeparator + ".."
-					+ Constants.innerFileSeparator + Constants.PLUGIN_XML_FILE);
-		}
-		final SAXReader reader = new SAXReader();
-		Document pluginXMLDocument = null;
-		reader.setValidation(false);
-		try {
-			pluginXMLDocument = reader.read(pluginXMLFile);
-		} catch (final DocumentException e) {
-			Activator.getDefault().logError(e);
-		}
-		if (pluginXMLDocument != null) {
-
-			final Element root = pluginXMLDocument.getRootElement();
-			for (final Iterator<Element> extensionElement = root.elementIterator("extension"); extensionElement
-					.hasNext();) {
-				final Element currentExtensionElement = extensionElement.next();
-				final Attribute point = currentExtensionElement.attribute("point");
-				if (point != null && point.getValue().equals("org.eclipse.help.contexts")) {
-					currentExtensionElement.addElement("contexts").addAttribute("file",
-							Constants.HELP_FILE_DIRECTORY_PATH + machineReadableTaskName + Constants.XML_EXTENSION);
-				}
-			}
-
-			try (FileWriter fileWriter = new FileWriter(pluginXMLFile)) {
-				final OutputFormat format = OutputFormat.createPrettyPrint();
-				final XMLWriter writer = new XMLWriter(fileWriter, format);
-				writer.write(pluginXMLDocument);
-				writer.close();
-			} catch (final IOException e) {
-				Activator.getDefault().logError(e);
-			}
-		}
-	}
-
+	
+	
 	/**
 	 * Return the name of that task that is set for the file writes..
 	 *
--- plugins/de.cognicrypt.integrator.task/src/main/java/de/cognicrypt/integrator/task/wizard/TaskIntegratorWizard.java
+++ plugins/de.cognicrypt.integrator.task/src/main/java/de/cognicrypt/integrator/task/wizard/TaskIntegratorWizard.java
@@ -17,7 +17,6 @@ import java.util.HashMap;
 import java.util.List;
 
 import org.eclipse.core.runtime.Platform;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jface.wizard.Wizard;
@@ -36,7 +35,7 @@ import de.cognicrypt.integrator.task.models.IntegratorModel;
 
 public class TaskIntegratorWizard extends Wizard {
 	
-	private TaskIntegratorWizardPage taskInformationPage;
+	private TaskIntegratorWizardPage taskInformation;
 	
 	public TaskIntegratorWizard() {
 		setWindowTitle("CogniCrypt Task Integrator");
@@ -47,8 +46,8 @@ public class TaskIntegratorWizard extends Wizard {
 
 	@Override
 	public void addPages() {
-		taskInformationPage = new TaskIntegratorWizardPage(Constants.PAGE_TASK_INFORMATION, Constants.PAGE_TITLE_FOR_MODE_OF_WIZARD, Constants.PAGE_DESCRIPTION_FOR_MODE_OF_WIZARD);
-		addPage(taskInformationPage);
+		taskInformation = new TaskIntegratorWizardPage(Constants.PAGE_TASK_INFORMATION, Constants.PAGE_TITLE_FOR_MODE_OF_WIZARD, Constants.PAGE_DESCRIPTION_FOR_MODE_OF_WIZARD);
+		addPage(taskInformation);
 		
 		addPage(new QuestionsPage());
 	}
@@ -58,10 +57,11 @@ public class TaskIntegratorWizard extends Wizard {
 	public boolean canFinish() {
 		
 		if(!IntegratorModel.getInstance().isGuidedModeChosen())
-			return taskInformationPage.checkNonGuidedFinish();
+			return taskInformation.checkNonGuidedFinish();
 	
-		if(IntegratorModel.getInstance().getIdentifiers().size() == 1)
-			return taskInformationPage.checkMandatoryFields();
+		if(IntegratorModel.getInstance().getIdentifiers().size() == 1) {
+			return taskInformation.checkMandatoryFields();
+		}
 			
 		return super.canFinish();
 	}
@@ -90,52 +90,33 @@ public class TaskIntegratorWizard extends Wizard {
 		final FileUtilities fileUtilities = new FileUtilities(integratorModel.getTaskName());
 		Task task = integratorModel.getTask();
 		HashMap<String, File> crylTemplatesWithOption = integratorModel.getCryslTemplateFiles();
-		if (getContainer().getCurrentPage().getName().equals(Constants.PAGE_TASK_INFORMATION)) {
-			String fileWriteAttemptResult;
-			if (integratorModel.isGuidedModeChosen() == false) {
-				 fileWriteAttemptResult = fileUtilities.writeCryslTemplate(crylTemplatesWithOption, integratorModel.getLocationOfJSONFile(), integratorModel.getLocationOfIconFile());}
-			else {
-				fileWriteAttemptResult = fileUtilities.writeCryslTemplate( (File) crylTemplatesWithOption.values().toArray()[0], integratorModel.getLocationOfIconFile());
+		String fileWriteAttemptResult;
+		
+		if(integratorModel.isGuidedModeChosen()) {
+			fileWriteAttemptResult = fileUtilities.writeData(crylTemplatesWithOption, integratorModel.getLocationOfIconFile());
+			if (integratorModel.getIdentifiers().size() == 1) {
 				fileUtilities.writeJSONFile(new ArrayList<Question>());
-			}
-				// Check if the contents of the provided files are valid.
-				if (fileWriteAttemptResult.equals("")) {
-					// Adding the trimmed task name to ensure it matches with the name of the image stored (refer FileUtilities)
-					task.setImage(task.getName().replaceAll("[^A-Za-z0-9]", ""));
-					task.setCodeGen(CodeGenerators.CrySL);
-					fileUtilities.writeTaskToJSONFile(task);
-					fileUtilities.updateThePluginXMLFileWithHelpData(integratorModel.getTaskName());
-					return true;
-				} else {
-					MessageDialog.openError(getShell(), "Warning", fileWriteAttemptResult);
-					return false;
-				}
-		} else {
-
-			// collect input to task-related files from individual pages
-			
-			final String fileWriteAttemptResult = fileUtilities.writeCryslTemplate(crylTemplatesWithOption,  integratorModel.getLocationOfIconFile());
-			final ArrayList<Question> questions = IntegratorModel.getInstance().getQuestions();
-			
-			// Check if the contents of the provided files are valid.
-			if (fileWriteAttemptResult.equals("")) {
-				// Adding the trimmed task name to ensure it matches with the name of the image stored (refer FileUtilities)
-				task.setImage(task.getName().replaceAll("[^A-Za-z0-9]", ""));
-				task.setCodeGen(CodeGenerators.CrySL);
-				task.setModelFile("");
-				task.setAdditionalResources("");
+			}else {
+				final ArrayList<Question> questions =
+						integratorModel.getQuestions();
 				fileUtilities.writeJSONFile(questions);
-				fileUtilities.writeTaskToJSONFile(task);
-				fileUtilities.updateThePluginXMLFileWithHelpData(integratorModel.getTaskName());
-			} else {
-				final MessageBox errorBox = new MessageBox(getShell(), SWT.ERROR | SWT.OK);
-				errorBox.setText("Problems with the provided files.");
-				errorBox.setMessage(fileWriteAttemptResult);
-				errorBox.open();
 			}
+		}else {
+			fileWriteAttemptResult = fileUtilities.writeData(crylTemplatesWithOption, integratorModel.getLocationOfIconFile(), integratorModel.getLocationOfJSONFile());
 		}
 		
-		return true;
+		if (fileWriteAttemptResult.equals("")) {
+			task.setImage(task.getName().replaceAll("[^A-Za-z0-9]", ""));
+			task.setCodeGen(CodeGenerators.CrySL);
+			fileUtilities.writeTaskToJSONFile(task);
+			return true;
+		}else {
+			final MessageBox errorBox = new MessageBox(getShell(), SWT.ERROR | SWT.OK);
+			errorBox.setText("Problems with the provided files.");
+			errorBox.setMessage(fileWriteAttemptResult);
+			errorBox.open();
+			return false;
+		}
 	}
 
 	
@@ -167,6 +148,7 @@ public class TaskIntegratorWizard extends Wizard {
 			writer.write("[]");
 			writer.close();
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
